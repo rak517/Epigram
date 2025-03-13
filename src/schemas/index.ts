ce0e607590481aecc9389343c1c03ecd7896f7a0
Tokyun02 <developtk02@gmail.com>
2025-03-13 15:46:55 +0900

    :bug: fix : 닉네임 초과 테스트 통과

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export const signupSchema = z.object({
   email: z.string().min(1, '이메일은 필수 입력입니다.').email('이메일 형식으로 작성해 주세요.'),
-  nickname: z.string().min(1, '닉네임은 필수 입력입니다.'),
+  nickname: z.string().min(1, '닉네임은 필수 입력입니다.').max(20, '닉네임은 최대 20자까지 가능합니다.'),
 });
