e09771d0db23d1c91f6cc7fdde187810b88169a9
CJewon <112820744+CJewon@users.noreply.github.com>
2025-03-13 23:38:46 +0900

    :art: art : header의 fixed 속성 추가

--- src/components/ui/header/LandingHeader.tsx
+++ src/components/ui/header/LandingHeader.tsx
@@ -9,10 +9,10 @@ export interface LandingHeaderProps {
   showIcon?: boolean;
 }
 
-export default function LandingHeader({ showIcon = false }: LandingHeaderProps) {
+export default function LandingHeader({ showIcon = true }: LandingHeaderProps) {
   if (showIcon) {
     return (
-      <header className='border-b-line-100 flex items-center justify-between border-b-[1px] px-6 py-2 md:h-[80px] md:py-3 xl:px-[72px] xl:px-[120px] xl:py-4'>
+      <header className='border-b-line-100 fixed flex w-full items-center justify-between border-b-[1px] px-6 py-2 md:h-[80px] md:py-3 xl:px-[72px] xl:px-[120px] xl:py-4'>
         <Link href={'/search'}>
           <Image src={searchIcon} alt='검색 아이콘' className='xl:w-9' />
         </Link>
@@ -26,7 +26,7 @@ export default function LandingHeader({ showIcon = false }: LandingHeaderProps) 
     );
   }
   return (
-    <header className='items-between border-b-line-100 flex justify-center border-b-[1px] py-[13px] xl:h-[80px]'>
+    <header className='items-between border-b-line-100 fixed flex w-full justify-center border-b-[1px] py-[13px] xl:h-[80px]'>
       <Link href={'/'} className='flex items-center justify-center'>
         <Image src={headerLogo} alt='헤더 로고' className='xl:h-9 xl:w-[131px]' />
       </Link>
