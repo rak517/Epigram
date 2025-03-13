b0826518d903fea0c7407293da37ea7a7e205e3f
Tokyun02 <developtk02@gmail.com>
2025-03-13 23:11:25 +0900

    :sparkles: feat : 소셜 로그인 기능 구현

--- src/components/auth/SocialButtonGroup.tsx
+++ src/components/auth/SocialButtonGroup.tsx
@@ -1,8 +1,27 @@
+'use client';
+
 import Image from 'next/image';
 import GoogleLogo from '@/assets/images/logo_google.svg';
 import KakaoLogo from '@/assets/images/logo_kakao.svg';
 
 export default function SocialButtonGroup() {
+  const handleSocialLogin = (provider: 'google' | 'kakao') => {
+    if (provider === 'kakao') {
+      const KAKAO_OAUTH_URL =
+        'https://kauth.kakao.com/oauth/authorize?client_id=' + process.env.NEXT_PUBLIC_KAKAO_API_KEY! + '&redirect_uri=' + process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI! + '&response_type=code';
+      window.location.href = KAKAO_OAUTH_URL;
+    } else {
+      const GOOGLE_OAUTH_URL =
+        `https://accounts.google.com/o/oauth2/v2/auth?` +
+        `client_id=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&` +
+        `redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&` +
+        `response_type=code&` +
+        `scope=openid%20profile%20email&` +
+        `state=google-login-state`;
+      window.location.href = GOOGLE_OAUTH_URL;
+    }
+  };
+
   return (
     <div className='w-full max-w-96 space-y-2'>
       <div className='flex w-full items-center'>
@@ -11,8 +30,8 @@ export default function SocialButtonGroup() {
         <hr className='flex-grow border-t border-gray-200' />
       </div>
       <div className='flex w-full items-center justify-center gap-2'>
-        <Image src={GoogleLogo} alt='구글 소셜 로그인' width={40} height={40} />
-        <Image src={KakaoLogo} alt='카카오 소셜 로그인' width={40} height={40} />
+        <Image src={GoogleLogo} alt='구글 소셜 로그인' width={40} height={40} onClick={() => handleSocialLogin('google')} className='cursor-pointer' />
+        <Image src={KakaoLogo} alt='카카오 소셜 로그인' width={40} height={40} onClick={() => handleSocialLogin('kakao')} className='cursor-pointer' />
       </div>
     </div>
   );
