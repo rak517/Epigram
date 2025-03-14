3ce908b02fee2af5d3661d83d92474d36931c1e2
Tokyun02 <developtk02@gmail.com>
2025-03-14 14:52:21 +0900

    :sparkles: feat : redirect 구현

---
+++ src/constants/route.ts
@@ -0,0 +1,2 @@
+export const BEFORE_LOGIN_ROUTE = ['/signup', '/login'] satisfies readonly string[];
+export const AFTER_LOGIN_ROUTE = ['/epigrams', '/feed', '/search', '/addepigram', '/mypage'] satisfies readonly string[];
