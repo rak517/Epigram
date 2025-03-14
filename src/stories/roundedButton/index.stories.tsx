d2350d0049a1a21bed5e8714f05e0a46a969a728
jaehyeongjung <titochung625@naver.com>
2025-03-15 00:39:26 +0900

    :sparkles: feat:공용 rounded-button 컴포넌트 제작

---
+++ src/stories/roundedButton/index.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import RoundedButton, { RoundedButtonProps } from '@/components/ui/roundedButton'; 
+
+export default {
+  title: 'Components/RoundedButton',
+  component: RoundedButton,
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['더보기', '에픽그램', '좋아요버튼', '왕도로가는길', '커스텀'],
+    },
+    size: {
+      control: 'select',
+      options: ['small', 'medium'],
+    },
+    backgroundColor: {
+      control: 'color',
+      if: { arg: 'type', eq: '커스텀' },
+    },
+    text: {
+      control: 'text',
+      if: { arg: 'type', eq: '커스텀' },
+    },
+  },
+} as Meta<typeof RoundedButton>;
+
+const Template: StoryFn<RoundedButtonProps> = (args) => <RoundedButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  type: '더보기',
+  size: 'medium',
+};
+
+export const Custom = Template.bind({});
+Custom.args = {
+  type: '커스텀',
+  size: 'medium',
+  backgroundColor: 'blue',
+  text: '사용자 지정',
+};
