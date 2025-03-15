b15fdddff1857ead237ab309645eb35d930bcfb5
송형진 <sori4606@naver.com>
2025-03-16 02:04:58 +0900

    :sparkles: feat : textcard 공용 컴포넌트 구현

---
+++ src/components/ui/textcard/index.tsx
@@ -0,0 +1,165 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import { HTMLAttributes } from 'react';
+import { Iropke } from '@/fonts';
+import dropdown_icon from '@/assets/icons/dropdown_icon.svg';
+import Image from 'next/image';
+
+const textCardVariants = cva(`border border-line-100 border-solid rounded-xl font-normal ${Iropke.className}`, {
+  variants: {
+    variant: {
+      fixedHeight: '',
+      variableHeight: 'min-h-[106px]',
+    },
+
+    width: {
+      w286: 'w-[286px]',
+      w294: 'w-[294px]',
+      w312: 'w-[312px]',
+      w366: 'w-[366px]',
+      w384: 'w-[384px]',
+      w472: 'w-[472px]',
+      w540: 'w-[540px]',
+      w585: 'w-[585px]',
+      w640: 'w-[640px]',
+      w744: 'w-[744px]',
+    },
+
+    fixedSize: {
+      w294h180: 'w-[294px] h-[180px]',
+      w312h140: 'w-[312px] h-[140px]',
+      w585h259: 'w-[585px] h-[259px]',
+    },
+
+    tagPosition: {
+      topLeft: '',
+      bottomRight: '',
+    },
+
+    fontSize: {
+      xs: 'text-xs',
+      sm: 'text-sm',
+      base: 'text-base',
+      xl: 'text-xl',
+      '2xl': 'text-2xl',
+    },
+  },
+});
+
+type DropdownSizeType = 'wh24' | 'wh36';
+
+const getDropdownSize = (size: DropdownSizeType | undefined) => {
+  switch (size) {
+    case 'wh24':
+      return { width: 24, height: 24 };
+    case 'wh36':
+      return { width: 36, height: 36 };
+    default:
+      return { width: 24, height: 24 };
+  }
+};
+
+interface TextCardProps extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof textCardVariants> {
+  isDropdown?: boolean;
+  cardContent?: string;
+  author?: string;
+  tags?: string[];
+  maxTags?: number; // 최대 표시할 태그 수
+  dropdownSize?: DropdownSizeType;
+}
+
+export default function TextCard({ isDropdown, cardContent, author, tags = [], maxTags = 2, variant, width, fixedSize, fontSize, tagPosition, dropdownSize, className, ...props }: TextCardProps) {
+  let fixedSizeClass = '';
+  if (fixedSize === 'w294h180') fixedSizeClass = 'w-[294px] h-[180px]';
+  else if (fixedSize === 'w312h140') fixedSizeClass = 'w-[312px] h-[140px]';
+  else if (fixedSize === 'w585h259') fixedSizeClass = 'w-[585px] h-[259px]';
+
+  const displayTags = tags.length > maxTags ? [...tags.slice(0, maxTags), '...'] : tags;
+
+  // 드롭다운 아이콘 사이즈
+  const iconSize = getDropdownSize(dropdownSize);
+
+  if (fixedSize) {
+    return (
+      <div
+        className={`${textCardVariants({
+          variant,
+          width,
+          fontSize,
+          tagPosition,
+          className,
+        })} relative mx-auto`}
+        {...props}
+      >
+        <div className={`${fixedSizeClass} relative`}>
+          <div className='p-[22px]'>
+            <div className='line-clamp-4'>{cardContent}</div>
+
+            {author && <div className='absolute right-5 bottom-5 text-blue-400'>- {author} -</div>}
+          </div>
+          {isDropdown && (
+            <div className='absolute top-4 right-4 cursor-pointer'>
+              <Image src={dropdown_icon} alt='드롭다운 메뉴 아이콘' width={iconSize.width} height={iconSize.height} />
+            </div>
+          )}
+        </div>
+        {tags.length > 0 && (
+          <div
+            className={`${tagPosition === 'topLeft' ? 'absolute left-0' : 'absolute right-0 text-right'} flex flex-row flex-wrap text-blue-400`}
+            style={{
+              top: tagPosition === 'topLeft' ? `calc(-1.5em - 12px)` : undefined,
+              bottom: tagPosition === 'bottomRight' ? `calc(-1.5em - 12px)` : undefined,
+            }}
+          >
+            {displayTags.map((tag, index) => (
+              <span key={index} className='px-2 py-1 text-blue-400'>
+                {tag === '...' ? tag : `#${tag}`}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className={`${textCardVariants({
+        variant,
+        width,
+        fixedSize,
+        fontSize,
+        tagPosition,
+        className,
+      })} relative mx-auto`}
+      {...props}
+    >
+      <div>
+        <div className='p-[22px]'>
+          <div className='mb-4'>{cardContent}</div>
+
+          {author && <div className='text-right text-blue-400'>- {author} -</div>}
+        </div>
+        {isDropdown && (
+          <div className='absolute top-4 right-4 cursor-pointer'>
+            <Image src={dropdown_icon} alt='드롭다운 메뉴 아이콘' width={iconSize.width} height={iconSize.height} />
+          </div>
+        )}
+      </div>
+      {tags.length > 0 && (
+        <div
+          className={`${tagPosition === 'topLeft' ? 'absolute left-0' : 'absolute right-0 text-right'} text-blue-400`}
+          style={{
+            top: tagPosition === 'topLeft' ? `calc(-1.5em - 12px)` : undefined,
+            bottom: tagPosition === 'bottomRight' ? `calc(-1.5em - 12px)` : undefined,
+          }}
+        >
+          {displayTags.map((tag, index) => (
+            <span key={index} className='px-2 py-1 text-blue-400'>
+              {tag === '...' ? tag : `#${tag}`}
+            </span>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/stories/textCard/index.stories.tsx
@@ -0,0 +1,160 @@
+import TextCard from '@/components/ui/textcard';
+import { Iropke } from '@/fonts';
+import { cn } from '@/utils/cn';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof TextCard> = {
+  title: 'TextCard/TextCard',
+  component: TextCard,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+
+  decorators: [
+    (Story) => (
+      <div className={cn('rounded-xl bg-white', Iropke.className)}>
+        <Story />
+      </div>
+    ),
+  ],
+
+  argTypes: {
+    variant: {
+      options: ['fixedHeight', 'variantHeight'],
+      control: { type: 'select' },
+    },
+
+    width: {
+      options: ['w286', 'w294', 'w312', 'w366', 'w384', 'w472', 'w540', 'w585', 'w640', 'w744'],
+      control: { type: 'select' },
+    },
+
+    fixedSize: {
+      options: ['w294h214', 'w312h172', 'w585h307'],
+      control: { type: 'select' },
+    },
+
+    tagPosition: {
+      options: ['topLeft', 'bottomRight'],
+      control: { type: 'select' },
+    },
+
+    fontSize: {
+      options: ['xs', 'sm', 'base', 'xl', '2xl'],
+      control: { type: 'select' },
+    },
+  },
+};
+
+type Story = StoryObj<typeof TextCard>;
+
+export default meta;
+
+export const variableDefault: Story = {
+  args: {
+    variant: 'variableHeight',
+    width: 'w286',
+    tagPosition: 'bottomRight',
+    fontSize: 'xs',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
+
+export const variableW312: Story = {
+  args: {
+    variant: 'variableHeight',
+    width: 'w312',
+    tagPosition: 'bottomRight',
+    fontSize: 'sm',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
+
+export const variableW384: Story = {
+  args: {
+    variant: 'variableHeight',
+    width: 'w384',
+    tagPosition: 'bottomRight',
+    fontSize: 'base',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
+
+export const variableW540: Story = {
+  args: {
+    variant: 'variableHeight',
+    width: 'w540',
+    tagPosition: 'bottomRight',
+    fontSize: 'xl',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
+
+export const variableW640: Story = {
+  args: {
+    variant: 'variableHeight',
+    width: 'w640',
+    tagPosition: 'bottomRight',
+    fontSize: 'xl',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
+
+export const variableW744: Story = {
+  args: {
+    variant: 'variableHeight',
+    width: 'w744',
+    tagPosition: 'bottomRight',
+    fontSize: '2xl',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
+
+export const fixedW294: Story = {
+  args: {
+    variant: 'fixedHeight',
+    fixedSize: 'w294h180',
+    tagPosition: 'bottomRight',
+    fontSize: 'base',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
+
+export const fixedW312: Story = {
+  args: {
+    variant: 'fixedHeight',
+    fixedSize: 'w312h140',
+    tagPosition: 'bottomRight',
+    fontSize: 'sm',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
+
+export const fixedW585: Story = {
+  args: {
+    variant: 'fixedHeight',
+    fixedSize: 'w585h259',
+    tagPosition: 'bottomRight',
+    fontSize: '2xl',
+    tags: ['태그내용', '나는 태그입니다'],
+    cardContent: '여기에 카드 내용이 들어갑니다.',
+    author: '저자 이름',
+  },
+};
