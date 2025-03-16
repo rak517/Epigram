8a4a85040a0fca16a4c000a5cbf56d0cbf4fe334
CJewon <112820744+CJewon@users.noreply.github.com>
2025-03-16 20:53:40 +0900

    :sparkles: feat : emotion button 관련 storybook 구현 및 global.css 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,9 +1,3 @@
-import EmotionButton from './components/EmotionButton';
-
 export default function MyPage() {
-  return (
-    <div>
-      <EmotionButton emotion='기쁨'></EmotionButton>
-    </div>
-  );
+  return <div>my page</div>;
 }
---
+++ src/stories/emotion/EmotionButton.stories.tsx
@@ -0,0 +1,43 @@
+import EmotionButton from '@/app/mypage/components/EmotionButton';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof EmotionButton> = {
+  title: 'Emotion/EmotionButton',
+  component: EmotionButton,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    (Story) => (
+      <div>
+        <Story></Story>
+      </div>
+    ),
+  ],
+  argTypes: {
+    buttonVariant: {
+      options: ['default', 'onSelect'],
+      control: { type: 'select' },
+    },
+    size: {
+      options: ['sm', 'md', 'lg', 'xl'],
+      control: { type: 'select' },
+    },
+    emotion: {
+      options: ['기쁨', '슬픔', '감동', '고민', '분노'],
+      control: { type: 'select' },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof EmotionButton>;
+
+export const Default: Story = {
+  args: {
+    buttonVariant: 'default',
+    size: 'sm',
+    emotion: '기쁨',
+  },
+};
