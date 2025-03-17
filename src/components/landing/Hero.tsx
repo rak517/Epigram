9edcf56825369012f2af67ff3341a41ce1738ea5
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-03-17 17:22:29 +0900

    :sparkles: feat: Hero 컴포넌트 구현

---
+++ src/components/landing/Hero.tsx
@@ -0,0 +1,64 @@
+import { Iropke } from '@/fonts';
+import { cn } from '@/utils/cn';
+import Image from 'next/image';
+import arrow from '@/assets/icons/ic_up.svg';
+import Link from 'next/link';
+import { motion } from 'motion/react';
+import { easeInOut } from 'motion';
+import { useTypingEffect } from '@/hooks/useTypingEffect';
+
+const containerVariants = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const childVariants = {
+  initial: { y: 20, opacity: 0 },
+  animate: { y: 0, opacity: 1, transition: { ease: easeInOut } },
+};
+
+interface HeroProps {
+  handleScroll: () => void;
+}
+
+const MotionLink = motion.create(Link);
+
+export default function Hero({ handleScroll }: HeroProps) {
+  const FULL_TEXT = `나만 갖고 있기엔\n아까운 글이 있지 않나요?`;
+
+  const typedText = useTypingEffect(FULL_TEXT, 100);
+  return (
+    <motion.div
+      initial='initial'
+      whileInView='animate'
+      variants={containerVariants}
+      className='after:zigzag px relative flex h-[672px] flex-col items-center justify-center bg-[repeating-linear-gradient(to_bottom,white_0px,white_24px,#f2f2f2_25px)] md:h-[674px] lg:h-[1040px]'
+    >
+      <div className='flex flex-col items-center gap-6 md:gap-8 lg:gap-12'>
+        <div className={cn('flex flex-col items-center gap-2 md:gap-6 lg:gap-10', Iropke.className)}>
+          <h2 className='text-black-500 h-[90px] text-center text-2xl whitespace-pre-wrap md:text-3xl lg:text-4xl'>{typedText}</h2>
+          <span className='text-black-300 text-md md:text-xl'>다른 사람들과 감정을 공유해보세요.</span>
+        </div>
+        <MotionLink
+          variants={childVariants}
+          href='/epigrams'
+          className='bg-black-500 flex h-12 w-[112px] items-center justify-center rounded-xl px-4 text-lg font-semibold text-blue-100 lg:h-16 lg:w-[286px] lg:text-xl'
+        >
+          시작하기
+        </MotionLink>
+      </div>
+
+      <div className='absolute bottom-18 flex flex-col items-center gap-1'>
+        <span className='cursor-pointer text-xs font-semibold text-blue-400 md:text-lg' onClick={handleScroll}>
+          더 알아보기{' '}
+        </span>
+        <Image src={arrow} width={24} height={24} alt='더 알아보기' />
+      </div>
+    </motion.div>
+  );
+}
