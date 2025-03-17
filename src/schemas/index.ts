ffebfb153fdba3283ba29cbd42457dc919d86eef
Tokyun02 <developtk02@gmail.com>
2025-03-17 13:58:31 +0900

    :bug: fix : 이메일 형식 테스트 통과

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -42,5 +42,5 @@ export const userResponseSchema = z.object({
 });
 
 export const loginSchema = z.object({
-  email: z.string().min(1, '이메일은 필수 입력입니다.'),
+  email: z.string().min(1, '이메일은 필수 입력입니다.').email('이메일 형식으로 작성해 주세요.'),
 });
