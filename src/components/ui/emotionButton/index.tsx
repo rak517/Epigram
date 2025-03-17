2353d0a4c31df82fabc0defb2797d5bd9445c2da
CJewon <112820744+CJewon@users.noreply.github.com>
2025-03-17 16:39:55 +0900

    :sparkles: feat : 마이페이지 emotion button 컴포넌트 구현 (#53)
    * :sparkles: feat : 마이페이지 emotion button 컴포넌트 구현
    
    * :sparkles: feat : emotion button 관련 storybook 구현 및 global.css 수정
    
    * :bug: bug : dev 브랜치와의 conflict 해결 커밋
    
    * :recycle: refactor : 타입 좁히기를 통해 emotion에 잘못된 값 입력 방지

---
+++ src/components/ui/emotionButton/index.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import Emotion, { EmotionProps } from '@/components/ui/emotion';
+import { cn } from '@/utils/cn';
+import { cva } from 'class-variance-authority';
+import { useState } from 'react';
+
+type EmotionType = '감동' | '기쁨' | '슬픔' | '고민' | '분노';
+
+interface EmotionButtonProps {
+  buttonVariant?: 'default' | 'onSelect';
+  size?: 'sm' | 'md' | 'lg' | 'xl';
+  emotion: EmotionType;
+  emotionVariant?: EmotionProps['variant'];
+
+  onClick?: () => void;
+}
+
+const buttonStyles = cva('flex justify-center items-center rounded-2xl cursor-pointer', {
+  variants: {
+    buttonVariant: {
+      default: 'bg-blue-gray-100',
+      onSelect: 'border-3',
+    },
+    size: {
+      sm: 'w-14 h-14',
+      md: 'w-16 h-16',
+      lg: 'w-18 h-18',
+      xl: 'w-24 h-24',
+    },
+  },
+  defaultVariants: {
+    buttonVariant: 'default',
+    size: 'sm',
+  },
+});
+
+const buttonBorderMap = {
+  감동: 'border-illust-yellow',
+  기쁨: 'border-illust-green',
+  슬픔: 'border-illust-purple',
+  고민: 'border-illust-blue',
+  분노: 'border-illust-red',
+} as const;
+
+export default function EmotionButton({ buttonVariant, emotion, emotionVariant, size, onClick }: EmotionButtonProps) {
+  const [currentButtonVariant, setCurrentButtonVariant] = useState(buttonVariant || 'default');
+
+  const buttonBorderColor = buttonBorderMap[emotion];
+
+  const handleClick = () => {
+    setCurrentButtonVariant(currentButtonVariant === 'default' ? 'onSelect' : 'default');
+    onClick?.();
+  };
+  return (
+    <button className={cn(buttonStyles({ buttonVariant: currentButtonVariant, size }), buttonBorderColor)} onClick={handleClick}>
+      <Emotion variant={emotionVariant} emotion={emotion} size={size} />
+    </button>
+  );
+}
