26240d121974b3215b623bcf8c45ba4264d7f6e5
Tokyun02 <developtk02@gmail.com>
2025-03-17 13:35:25 +0900

    :wrench: chore : google 및 kakao 이미지 config 등록

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { useGetUser } from '@/apis/user/queries';
+
 export default function Home() {
-  return <div>landing Page</div>;
+  const { data } = useGetUser();
+  return <div>landing Page {JSON.stringify(data)}</div>;
 }
