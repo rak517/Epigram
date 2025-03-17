47de16664941a6de9047cd92bd1634476ee44d3c
Tokyun02 <developtk02@gmail.com>
2025-03-17 14:29:56 +0900

    :sparkles: feat : 로그인 기능 구현

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -4,11 +4,15 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { loginSchema } from '@/schemas';
 import { LoginForm as LoginFormType } from '@/types';
 import Input from '@/components/ui/Field/Input';
 import Button from '@/components/ui/buttons';
+import { useModalStore } from '@/stores/ModalStore';
+import { getErrorMessage } from '@/utils/network/getErrorMessage';
+import { useLogin } from '@/apis/auth/queries';
 import OpendEye from '@/assets/icons/opend_eye.svg';
 import ClosedEye from '@/assets/icons/closed_eye.svg';
 import Logo from '@/assets/images/logo.png';
@@ -16,6 +20,8 @@ import Logo from '@/assets/images/logo.png';
 export default function LoginForm() {
   const {
     register,
+    handleSubmit,
+    reset,
     formState: { errors, isDirty, isValid, isSubmitting },
   } = useForm<LoginFormType>({
     resolver: zodResolver(loginSchema),
@@ -26,11 +32,37 @@ export default function LoginForm() {
     },
   });
 
+  const { mutateAsync: login } = useLogin();
+  const { openModal } = useModalStore();
+  const router = useRouter();
+
   const [isShowPassword, setIsShowPassword] = useState(true);
   const isDisabled = !isDirty || !isValid || isSubmitting;
 
+  const onSubmit = async (loginForm: LoginFormType) => {
+    try {
+      await login(loginForm);
+      reset();
+      openModal({
+        type: 'alert',
+        title: '로그인에 성공했습니다!',
+        description: '확인 버튼을 누르시면 홈페이지로 이동합니다.',
+        callback: () => {
+          router.push('/');
+        },
+      });
+    } catch (error) {
+      const errorMessage = getErrorMessage(error);
+      openModal({
+        type: 'alert',
+        title: '회원가입에 실패했습니다.',
+        description: errorMessage,
+      });
+    }
+  };
+
   return (
-    <form className='flex w-full max-w-96 flex-col gap-2'>
+    <form className='flex w-full max-w-96 flex-col gap-2' onSubmit={handleSubmit(onSubmit)}>
       <Link className='flex w-full items-center justify-center' href='/'>
         <Image src={Logo} alt='로고' width={172} height={48} className='cursor-pointer' />
       </Link>
