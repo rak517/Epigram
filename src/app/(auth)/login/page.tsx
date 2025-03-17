86b275ae1e0a477656194477ad6211ba28d915fb
Tokyun02 <developtk02@gmail.com>
2025-03-17 15:37:02 +0900

    :sparkles: feat : login 페이지 error params 핸들링

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,31 @@
+'use client';
+
 import LoginForm from '@/components/auth/LoginForm';
 import SocialButtonGroup from '@/components/auth/SocialButtonGroup';
+import { useModalStore } from '@/stores/ModalStore';
 import Link from 'next/link';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Login() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const errorMessage = searchParams.get('error');
+  const { openModal } = useModalStore();
+
+  useEffect(() => {
+    if (errorMessage) {
+      openModal({
+        type: 'alert',
+        title: '오류가 발생했습니다.',
+        description: errorMessage,
+        callback: () => {
+          router.replace('/login');
+        },
+      });
+    }
+  }, [errorMessage, openModal, router]);
+
   return (
     <div className='flex w-full flex-col items-center justify-center gap-2 px-4'>
       <LoginForm />
