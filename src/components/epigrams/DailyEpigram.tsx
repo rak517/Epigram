b3dba84ae9250139e613055d1da0a27ab598e3a2
Tokyun02 <developtk02@gmail.com>
2025-03-18 18:28:03 +0900

    :sparkles: feat : Daily Epigram 컴포넌트 구현

---
+++ src/components/epigrams/DailyEpigram.tsx
@@ -0,0 +1,21 @@
+import { EpigramResponse } from '@/types';
+import TextCard from '@/components/ui/textcard';
+
+const REVALIDATE_TIME = 60 * 10;
+
+export default async function DailyEpgiram() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/epigrams/today`, {
+    next: {
+      revalidate: REVALIDATE_TIME,
+    },
+  });
+
+  const epigramData: EpigramResponse = await response.json();
+
+  return (
+    <section className='flex flex-col gap-6 text-lg md:text-2xl'>
+      <h2 className='text-black-600 font-semibold'>오늘의 에피그램</h2>
+      <TextCard cardContent={epigramData.content} author={epigramData.author} tags={epigramData.tags} />
+    </section>
+  );
+}
