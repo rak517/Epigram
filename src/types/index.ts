b2e5b204ec3e83ae0f68362ca17f9c850e4a7099
Tokyun02 <developtk02@gmail.com>
2025-03-18 18:27:24 +0900

    :sparkles: feat : 에피그램 스키마 및 타입 정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { loginSchema, signupSchema, userSchema } from '@/schemas';
+import { epigramResponseSchema, loginSchema, signupSchema, tagSchema, userSchema } from '@/schemas';
 
 export type SignupForm = z.infer<typeof signupSchema>;
 
@@ -7,7 +7,6 @@ export type User = z.infer<typeof userSchema>;
 
 export type LoginForm = z.infer<typeof loginSchema>;
 
-export interface Tag {
-  id: number;
-  name: string;
-}
+export type Tag = z.infer<typeof tagSchema>;
+
+export type EpigramResponse = z.infer<typeof epigramResponseSchema>;
