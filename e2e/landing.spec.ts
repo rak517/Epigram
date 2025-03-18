c622a5665d24efbf9739647f1d6e149ddf73ad09
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-03-18 20:28:08 +0900

    :recycle: refactor: 랜딩페이지 E2E 코드 리뷰 반영 및 beforeEach로 로직 변경

--- e2e/landing.spec.ts
+++ e2e/landing.spec.ts
@@ -1,61 +1,66 @@
 import { test, expect, BrowserContext, Page } from '@playwright/test';
 
 test.describe('랜딩 페이지', () => {
-  test('랜딩 페이지 로드 확인', async ({ page }) => {
+  let context: BrowserContext;
+  let page: Page;
+
+  test.beforeEach(async ({ browser }) => {
+    context = await browser.newContext();
+    page = await context.newPage();
     await page.goto('/');
+  });
+
+  test.afterEach(async () => {
+    await context.close();
+  });
+
+  test('랜딩 페이지 로드 확인', async () => {
     await expect(page).toHaveURL('http://localhost:3000');
   });
 
-  test('로그인이 안되어 있으면, 검색 아이콘 클릭 시 로그인 페이지로 이동한다', async ({ page }) => {
-    await page.goto('/');
+  test('로그인이 안되어 있으면, 검색 아이콘 클릭 시 로그인 페이지로 이동한다', async () => {
     await page.getByAltText('검색 아이콘').click();
     await expect(page).toHaveURL('http://localhost:3000/login');
   });
 
-  test('로고 클릭 시 랜딩 페이지로 이동한다.', async ({ page }) => {
-    await page.goto('/');
+  test('로고 클릭 시 랜딩 페이지로 이동한다.', async () => {
     await page.getByAltText('헤더 로고').click();
     await expect(page).toHaveURL('http://localhost:3000');
   });
 
-  test('로그인이 안되어 있으면, 유저 아이콘 클릭 시 로그인 페이지로 이동한다.', async ({ page }) => {
-    await page.goto('/');
+  test('로그인이 안되어 있으면, 유저 아이콘 클릭 시 로그인 페이지로 이동한다.', async () => {
     await page.getByAltText('유저 아이콘').click();
     await expect(page).toHaveURL('http://localhost:3000/login');
   });
 
-  test('로그인이 안되어 있으면, 시작하기 텍스트 클릭 시 로그인 페이지로 이동한다', async ({ page }) => {
-    await page.goto('/');
+  test('로그인이 안되어 있으면, 시작하기 텍스트 클릭 시 로그인 페이지로 이동한다', async () => {
     await page.getByRole('link', { name: '시작하기' }).first().click();
     await expect(page).toHaveURL('http://localhost:3000/login');
   });
 
-  test('로그인이 안되어 있으면, 하단의 시작하기 텍스트 클릭 시 로그인 페이지로 이동한다', async ({ page }) => {
-    await page.goto('/');
+  test('로그인이 안되어 있으면, 하단의 시작하기 텍스트 클릭 시 로그인 페이지로 이동한다', async () => {
     await page.getByRole('link', { name: '시작하기' }).nth(1).click();
     await expect(page).toHaveURL('http://localhost:3000/login');
   });
 
-  test('더 알아보기 텍스트 클릭 시 기능 소개 레이아웃으로 스크롤이 내려간다', async ({ page }) => {
+  test('더 알아보기 텍스트 클릭 시 기능 소개 레이아웃으로 스크롤이 내려간다', async () => {
     const initialScrollY = await page.evaluate(() => window.scrollY);
 
-    await page.goto('/');
     await page.getByText('더 알아보기').click();
 
-    await page.waitForTimeout(300);
+    await page.waitForTimeout(1000);
 
     const newScrollY = await page.evaluate(() => window.scrollY);
 
     expect(newScrollY).toBeGreaterThan(initialScrollY);
   });
 
-  test('하단 화살표 이미지 클릭 시 기능 소개 레이아웃으로 스크롤이 내려간다', async ({ page }) => {
+  test('하단 화살표 이미지 클릭 시 기능 소개 레이아웃으로 스크롤이 내려간다', async () => {
     const initialScrollY = await page.evaluate(() => window.scrollY);
 
-    await page.goto('/');
     await page.getByAltText('더 알아보기').click();
 
-    await page.waitForTimeout(300);
+    await page.waitForTimeout(1000);
 
     const newScrollY = await page.evaluate(() => window.scrollY);
 
@@ -67,7 +72,7 @@ test.describe('로그인이 되어있는 랜딩페이지', () => {
   let context: BrowserContext;
   let page: Page;
 
-  test.beforeAll(async ({ browser }) => {
+  test.beforeEach(async ({ browser }) => {
     context = await browser.newContext();
     await context.addCookies([
       {
@@ -90,37 +95,33 @@ test.describe('로그인이 되어있는 랜딩페이지', () => {
       },
     ]);
     page = await context.newPage();
+    await page.goto('/');
   });
 
-  test.afterAll(async () => {
+  test.afterEach(async () => {
     await context.close();
   });
 
-  test('랜딩 페이지 로드 확인', async ({ page }) => {
-    await page.goto('/');
+  test('랜딩 페이지 로드 확인', async () => {
     await expect(page).toHaveURL('http://localhost:3000');
   });
 
   test('로그인이 되어 있으면, 검색 아이콘 클릭 시 검색 페이지로 이동한다', async () => {
-    await page.goto('/');
     await page.getByAltText('검색 아이콘').click();
     await expect(page).toHaveURL('http://localhost:3000/search');
   });
 
   test('로그인이 되어 있으면, 유저 아이콘 클릭 시 마이 페이지로 이동한다', async () => {
-    await page.goto('/');
     await page.getByAltText('유저 아이콘').click();
     await expect(page).toHaveURL('http://localhost:3000/mypage');
   });
 
   test('로그인이 되어 있으면 시작하기 텍스트 클릭 시 에피그램 페이지로 이동한다', async () => {
-    await page.goto('/');
     await page.getByRole('link', { name: '시작하기' }).first().click();
     await expect(page).toHaveURL('http://localhost:3000/epigrams');
   });
 
   test('로그인이 되어 있으면, 페이지 하단의 시작하기 텍스트 클릭 시 에피그램 페이지로 이동한다', async () => {
-    await page.goto('/');
     await page.getByRole('link', { name: '시작하기' }).nth(1).click();
     await expect(page).toHaveURL('http://localhost:3000/epigrams');
   });
