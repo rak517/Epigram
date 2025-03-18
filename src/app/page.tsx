34645870d0ce96890178129c1445dcc1d81bdd70
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-03-18 13:49:23 +0900

    :twisted_rightwards_arrows: merge: 리베이스 중 충돌 과정 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+'use client';
+
+import Epigrams from '@/components/landing/Epigrams';
+import Feature from '@/components/landing/Feature';
+import Footer from '@/components/landing/Footer';
+import Hero from '@/components/landing/Hero';
+import { useRef } from 'react';
+
 export default function Home() {
-  return <div>landing Page</div>;
+  const targetRef = useRef<HTMLDivElement | null>(null);
+
+  const handleScroll = () => {
+    if (targetRef.current) {
+      targetRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className='bg-background-100 min-h-screen'>
+      <Hero handleScroll={handleScroll} />
+      <Feature ref={targetRef} />
+      <Epigrams />
+      <Footer />
+    </div>
+  );
 }
