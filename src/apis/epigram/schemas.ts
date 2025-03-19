1d31179747d209a3f371ab43666fa31391fa06e3
Tokyun02 <developtk02@gmail.com>
2025-03-19 17:53:03 +0900

    :sparkles: feat : epigram 스키마 작성

--- src/apis/epigram/schemas.ts
+++ src/apis/epigram/schemas.ts
@@ -2,10 +2,10 @@ import { z } from 'zod';
 
 export const tagSchema = z.object({
   id: z.number(),
-  name: z.string(),
+  name: z.string().max(10, '태그는 최대 10글자까지 가능합니다.'),
 });
 
-export const epigramResponseSchema = z.object({
+export const epigramSchema = z.object({
   likeCount: z.number(),
   id: z.number(),
   content: z.string(),
@@ -14,4 +14,48 @@ export const epigramResponseSchema = z.object({
   referenceUrl: z.string(),
   writerId: z.number(),
   tags: z.array(tagSchema),
+  isLiked: z.boolean().optional(),
+});
+
+export const epigramFormSchema = z.object({
+  tags: z.array(tagSchema).max(3, '태그는 최대 3개입니다.'),
+  referenceUrl: z.string().trim(),
+  referenceTitile: z.string().trim(),
+  author: z.string().trim().max(50, '50자 이내로 입력해주세요.'),
+  content: z.string().trim().min(1, '내용을 입력해주세요.'),
+});
+
+export const getEpigramsParamsSchema = z.object({
+  limit: z.number(),
+  cursor: z.number().optional(),
+  keyword: z.string().optional(),
+  writerId: z.number().optional(),
+});
+
+export const patchEpigramSchema = epigramFormSchema.partial();
+
+export const getCommentsParamsSchema = z.object({
+  limit: z.number(),
+  cursor: z.number().optional(),
+});
+
+//TODO: comment API 작업이 완료되면 하위 수정할 예정입니다.
+export const commentSchema = z.object({
+  epigramId: z.number(),
+  writer: z.object({
+    image: z.string().nullable(),
+    nickname: z.string(),
+    id: z.number(),
+  }),
+  updatedAt: z.string(),
+  createdAt: z.string(),
+  isPrivate: z.boolean(),
+  content: z.string(),
+  id: z.number(),
+});
+
+export const commentsResponseSchema = z.object({
+  totalCount: z.number(),
+  nextCursor: z.number().nullable(),
+  list: z.array(commentSchema),
 });
--- src/apis/epigram/types.ts
+++ src/apis/epigram/types.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
-import { epigramResponseSchema, tagSchema } from './schemas';
+import { epigramSchema, tagSchema } from './schemas';
 
 export type Tag = z.infer<typeof tagSchema>;
 
-export type EpigramResponse = z.infer<typeof epigramResponseSchema>;
+export type Epigram = z.infer<typeof epigramSchema>;
