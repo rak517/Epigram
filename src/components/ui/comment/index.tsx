66b65cc8c9069a2087462961a13ce040e30243b4
pss7 <124497122+pss7@users.noreply.github.com>
2025-03-19 16:15:09 +0900

    EP-24 댓글 컴포넌트 구현 (#58)
    * ✨ feat : 댓글 컴포넌트 구현 (#24)
    
    * � chore : 코드리뷰 코드 개선 반영 (#58)
    
    * � chore : 코드 리뷰 후 코드 개선 반영 (#58)
    
    * :wrench: 코드리뷰 후 코드개선 반영
    
    * :wrench: 코드리뷰 후 코드개선 반영
    
    * :wrench : chore: 디폴트이미지 환경변수로 설정 후 스토리북에 적용 (#58)
    
    ---------
    
    Co-authored-by: parksangseok <park0157@naver.com>

---
+++ src/components/ui/comment/index.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Avatar from "@/components/ui/avatars";
+
+interface CommentProps {
+  isOwnComment: boolean;
+  nickname: string;
+  commentTime: string;
+  content: string;
+  profileImage: string;
+  onEdit?: () => void;
+  onDelete?: () => void;
+}
+
+export default function Comment({
+  isOwnComment,
+  nickname,
+  commentTime,
+  content,
+  profileImage,
+  onEdit,
+  onDelete,
+  ...props
+}: CommentProps) {
+  return (
+    <div
+      className="flex max-w-[640px] bg-background-100 hover:shadow-lg transition-all duration-300 ease-in-out border-t border-line-200 py-4 sm:py-4 md:py-6 lg:py-8 px-6"
+      {...props}
+    >
+      {/* 왼쪽: 이미지 박스 */}
+      <div className="w-12 h-12 mr-4 rounded-full overflow-hidden">
+        <Avatar src={profileImage} alt={`${nickname}님의 프로필 이미지`}
+          size="default" />
+      </div>
+
+      {/* 오른쪽: 텍스트 박스 */}
+      <div className="flex-1 flex flex-col">
+        {/* 상단 박스: 닉네임, 댓글 시간, 수정/삭제 버튼 */}
+        <div className="flex justify-between items-center mb-2">
+          <div className="flex items-center">
+            <span className="mr-2 text-black-300 text-xs sm:text-xs md:text-md lg:text-lg">
+              {nickname}
+            </span>
+            <span className="text-black-300 text-xs sm:text-xs md:text-md lg:text-lg">
+              {commentTime}
+            </span>
+          </div>
+
+          {isOwnComment && (
+            <div className="flex space-x-4">
+              <button
+                className="cursor-pointer text-black-600 underline text-xs sm:text-xs md:text-md lg:text-lg"
+                onClick={onEdit}
+              >
+                수정
+              </button>
+              <button
+                className="cursor-pointer text-error underline text-xs sm:text-xs md:text-md lg:text-lg"
+                onClick={onDelete}
+              >
+                삭제
+              </button>
+            </div>
+          )}
+        </div>
+
+        {/* 댓글 내용 */}
+        <div className="text-black-700 text-md sm:text-md md:text-lg lg:text-xl">
+          {content}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/stories/comment/index.stories.tsx
@@ -0,0 +1,56 @@
+import Comment from '@/components/ui/comment';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const defaultImageUrl =
+  process.env.NEXT_PUBLIC_DEFAULT_IMAGE_URL;
+
+const meta: Meta<typeof Comment> = {
+  title: 'Components/Comment',
+  component: Comment,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    isOwnComment: {
+      control: 'boolean',
+    },
+    nickname: {
+      control: 'text',
+    },
+    commentTime: {
+      control: 'text',
+    },
+    content: {
+      control: 'text',
+    },
+    profileImage: {
+      control: 'text',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Comment>;
+
+export const Default: Story = {
+  args: {
+    isOwnComment: false,
+    nickname: '지킬 앤 하이드',
+    commentTime: '1시간 전',
+    content:
+      '오늘 하루 우울했었는데 덕분에 많은 힘 얻고 갑니다. 연금술사 책 다시 사서 오랜만에 읽어봐야겠어요!',
+    profileImage: defaultImageUrl, // 환경 변수 적용
+  },
+};
+
+export const OwnComment: Story = {
+  args: {
+    isOwnComment: true,
+    nickname: '지킬 앤 하이드',
+    commentTime: '1시간 전',
+    content:
+      '오늘 하루 우울했었는데 덕분에 많은 힘 얻고 갑니다. 연금술사 책 다시 사서 오랜만에 읽어봐야겠어요!',
+    profileImage: defaultImageUrl, // 환경 변수 적용
+  },
+};
