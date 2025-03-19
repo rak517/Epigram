66d5000bfa739cb72e8c7e0212debcde43bc4dfa
Tokyun02 <developtk02@gmail.com>
2025-03-19 17:56:35 +0900

    :sparkles: feat : epigram 타입 정의

--- src/apis/epigram/types.ts
+++ src/apis/epigram/types.ts
@@ -1,6 +1,19 @@
 import { z } from 'zod';
-import { epigramSchema, tagSchema } from './schemas';
+import { commentSchema, commentsResponseSchema, epigramFormSchema, epigramSchema, getCommentsParamsSchema, getEpigramsParamsSchema, patchEpigramSchema, tagSchema } from './schemas';
 
 export type Tag = z.infer<typeof tagSchema>;
 
 export type Epigram = z.infer<typeof epigramSchema>;
+
+export type EpigramForm = z.infer<typeof epigramFormSchema>;
+
+export type GetEpigramsParams = z.infer<typeof getEpigramsParamsSchema>;
+
+export type PatchEpigram = z.infer<typeof patchEpigramSchema>;
+
+export type GetCommentsParams = z.infer<typeof getCommentsParamsSchema>;
+
+//TODO: comment API 작업이 완료되면 하위 수정할 예정입니다.
+export type CommentSchema = z.infer<typeof commentSchema>;
+
+export type CommentsResponse = z.infer<typeof commentsResponseSchema>;
