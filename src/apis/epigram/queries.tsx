f10bb1c3bee2a0d193405c513307e42691d70c21
Tokyun02 <developtk02@gmail.com>
2025-03-19 19:28:58 +0900

    :sparkles: feat : epigram query hook 구현

---
+++ src/apis/epigram/queries.tsx
@@ -0,0 +1,96 @@
+import { useInfiniteQuery, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { Epigram, EpigramForm, GetCommentsParams, GetEpigramsParams, PatchEpigram } from './types';
+import { deleteEpigram, deleteEpigramFavorite, getEpigram, getEpigrams, patchEpigram, postEpigram, postEpigramFavorite } from '.';
+
+export const usePostEpigram = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (epigramForm: EpigramForm) => {
+      return postEpigram(epigramForm);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['epigrams'] });
+    },
+  });
+};
+
+export const useGetEpigrams = (epigramsParams: GetEpigramsParams) => {
+  return useInfiniteQuery({
+    queryKey: ['epigrams', epigramsParams],
+    queryFn: ({ pageParam }) => getEpigrams({ ...epigramsParams, cursor: pageParam }),
+    getNextPageParam: (lastPage) => lastPage.nextCursor || 0,
+    initialPageParam: 0,
+  });
+};
+
+export const useGetEpigram = (epigramId: Epigram['id']) => {
+  return useQuery({
+    queryKey: ['epigram', epigramId],
+    queryFn: () => getEpigram(epigramId),
+  });
+};
+
+export const usePatchEpigram = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ epigramId, epigram }: { epigramId: Epigram['id']; epigram: PatchEpigram }) => {
+      return patchEpigram(epigramId, epigram);
+    },
+    onSuccess: (epigram) => {
+      queryClient.invalidateQueries({ queryKey: ['epigram', epigram.id] });
+      queryClient.invalidateQueries({ queryKey: ['epigrams'] });
+    },
+  });
+};
+
+export const useDeleteEpigram = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (epigramId: Epigram['id']) => {
+      return deleteEpigram(epigramId);
+    },
+    onSuccess: (epigram) => {
+      queryClient.invalidateQueries({ queryKey: ['epigram', epigram.id] });
+      queryClient.invalidateQueries({ queryKey: ['epigrams'] });
+    },
+  });
+};
+
+export const usePostEpigramFavorite = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (epigramId: Epigram['id']) => {
+      return postEpigramFavorite(epigramId);
+    },
+    onSuccess: (epigram) => {
+      queryClient.invalidateQueries({ queryKey: ['epigram', epigram.id] });
+      queryClient.invalidateQueries({ queryKey: ['epigrams'] });
+    },
+  });
+};
+
+export const useDeleteEpigramFavorite = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (epigramId: Epigram['id']) => {
+      return deleteEpigramFavorite(epigramId);
+    },
+    onSuccess: (epigram) => {
+      queryClient.invalidateQueries({ queryKey: ['epigram', epigram.id] });
+      queryClient.invalidateQueries({ queryKey: ['epigrams'] });
+    },
+  });
+};
+
+export const useGetComments = (epigramId: Epigram['id'], commentsParams: GetCommentsParams) => {
+  return useInfiniteQuery({
+    queryKey: ['epigrams', 'epigram', epigramId, 'epigramComments'],
+    queryFn: ({ pageParam }) => getEpigrams({ ...commentsParams, cursor: pageParam }),
+    getNextPageParam: (lastPage) => lastPage.nextCursor || 0,
+    initialPageParam: 0,
+  });
+};
