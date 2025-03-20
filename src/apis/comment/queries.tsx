2cdb8e34669ea0aedbe96cbec12970d4b753c0b7
Tokyun02 <developtk02@gmail.com>
2025-03-20 15:40:14 +0900

    :recycle: refactor : 댓글 mutation 쿼리 키 초기화 추가

--- src/apis/comment/queries.tsx
+++ src/apis/comment/queries.tsx
@@ -18,6 +18,7 @@ export const usePostComment = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['comments'] });
       queryClient.invalidateQueries({ queryKey: ['epigramComments'] });
+      queryClient.invalidateQueries({ queryKey: ['userComments'] });
     },
   });
 };
@@ -29,6 +30,7 @@ export const useUpdateComment = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['comments'] });
       queryClient.invalidateQueries({ queryKey: ['epigramComments'] });
+      queryClient.invalidateQueries({ queryKey: ['userComments'] });
     },
   });
 };
@@ -40,6 +42,7 @@ export const useDeleteComment = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['comments'] });
       queryClient.invalidateQueries({ queryKey: ['epigramComments'] });
+      queryClient.invalidateQueries({ queryKey: ['userComments'] });
     },
   });
 };
