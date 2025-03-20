b76dfd189c9146b0a103a077d88f665cc36fb1aa
Tokyun02 <developtk02@gmail.com>
2025-03-20 19:49:57 +0900

    :recycle: refactor : main header 스타일 수정

--- src/components/ui/header/MainHeader.tsx
+++ src/components/ui/header/MainHeader.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 export default function MainHeader() {
   return (
-    <header className='border-b-line-100 fixed z-50 flex w-full items-center justify-between border-b-[1px] bg-white px-6 py-[13px] md:px-[72px] md:py-[17px] xl:px-[120px] xl:py-[22px]'>
+    <header className='border-b-line-100 fixed top-0 right-0 left-0 z-10 flex h-16 items-center justify-between border-b-[1px] bg-white px-6 py-[13px] md:px-[72px] md:py-[17px] xl:px-[120px] xl:py-[22px]'>
       <div className='flex items-center justify-center gap-3 md:gap-6 xl:gap-9'>
         <Image src={menuIcon} alt='메뉴 아이콘' className='md:hidden'></Image>
         <Link href={'/'}>
@@ -19,7 +19,7 @@ export default function MainHeader() {
           검색
         </Link>
       </div>
-      <Link href={'/myapage'} className='flex gap-1.5'>
+      <Link href={'/mypage'} className='flex gap-1.5'>
         <Image src={userIconLight} alt='유저 아이콘'></Image>
         <p className='xl:text-md text-sm font-medium text-gray-300'>김코드</p>
       </Link>
