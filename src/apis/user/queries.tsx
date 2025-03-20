004dd58e5a42ed4c3fecd74686d0a7540653f3fb
Tokyun02 <developtk02@gmail.com>
2025-03-20 15:39:44 +0900

    :sparkles: feat : user 서비스 함수 구현

--- src/apis/user/queries.tsx
+++ src/apis/user/queries.tsx
@@ -1,9 +1,39 @@
-import { useQuery } from '@tanstack/react-query';
-import { getUser } from '.';
+import { useInfiniteQuery, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { getComments, getDetailUser, getUser, patchUser } from '.';
+import { PatchUser, User } from './types';
+import { GetCommentsParams } from '../epigram/types';
 
 export const useGetUser = () => {
   return useQuery({
     queryKey: ['user'],
     queryFn: () => getUser(),
+  });
+};
+
+export const usePatchUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (user: PatchUser) => {
+      return patchUser(user);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+    },
+  });
+};
+
+export const useGetDetailUser = (userId: User['id']) => {
+  return useQuery({
+    queryKey: ['userDetail', userId],
+    queryFn: () => getDetailUser(userId),
+  });
+};
+
+export const useGetComments = (userId: User['id'], commentsParams: GetCommentsParams) => {
+  return useInfiniteQuery({
+    queryKey: ['userComments'],
+    queryFn: ({ pageParam }) => getComments(userId, { ...commentsParams, cursor: pageParam }),
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+    initialPageParam: 0,
   });
 };
--- src/apis/user/schemas.ts
+++ src/apis/user/schemas.ts
@@ -14,3 +14,5 @@ export const userSchema = z.object({
 export const userResponseSchema = z.object({
   user: userSchema,
 });
+
+export const patchUserSchema = userSchema.pick({ image: true, nickname: true }).partial();
