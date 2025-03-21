186e1ab64dc2d48bf497f1152705df2661f396a8
Tokyun02 <developtk02@gmail.com>
2025-03-21 16:27:19 +0900

    :sparkles: feat : 오늘의 감정(메인페이지) 구현

--- src/apis/emotion-log/queries.tsx
+++ src/apis/emotion-log/queries.tsx
@@ -1,31 +1,59 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { getMonthlyEmotionLogs, getTodayEmotionLog, postTodayEmotionLog } from "."
-import { EmotionRequset,  GetMonthlyEmotionLogs } from "./types"
-import { User } from "../user/types"
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { getMonthlyEmotionLogs, getTodayEmotionLog, postTodayEmotionLog } from '.';
+import { EmotionRequset, GetMonthlyEmotionLogs, TodayEmotionLogs } from './types';
+import { User } from '@/apis/user/types';
 
-export const useGetTodayEmotionLog = (userId : User['id']) => {
-    return useQuery({
-        queryKey : [ 'todayEmotionLog' ,userId ],
-        queryFn :  () => getTodayEmotionLog(userId),
-    })
-}
+export const useGetTodayEmotionLog = (userId: User['id']) => {
+  return useQuery({
+    queryKey: ['todayEmotionLog', userId],
+    queryFn: () => getTodayEmotionLog(userId),
+  });
+};
 
 export const usePostTodayEmotionLog = () => {
-    const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-    return useMutation({
-        mutationFn : (params : EmotionRequset) => {
-            return postTodayEmotionLog(params)
-        },
-        onSuccess : () => {
-            queryClient.invalidateQueries({ queryKey : ['todayEmotionLog']})
-        }
-    })
-}
+  return useMutation({
+    mutationFn: (params: EmotionRequset) => {
+      return postTodayEmotionLog(params);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todayEmotionLog'] });
+    },
+  });
+};
 
-export const useGetMonthlyEmotionLogs = (params : GetMonthlyEmotionLogs) => {
-    return useQuery({
-        queryKey : ['month', params],
-        queryFn : () => getMonthlyEmotionLogs(params),
-    })
-}
+export const useGetMonthlyEmotionLogs = (params: GetMonthlyEmotionLogs) => {
+  return useQuery({
+    queryKey: ['month', params],
+    queryFn: () => getMonthlyEmotionLogs(params),
+  });
+};
+
+export const useOptimisticEmotionLog = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ params }: { params: EmotionRequset; userId: User['id'] }) => {
+      return postTodayEmotionLog(params);
+    },
+    onMutate: async ({ params, userId }: { params: EmotionRequset; userId: User['id'] }) => {
+      const previousData: TodayEmotionLogs = queryClient.getQueryData(['todayEmotionLog', userId])!;
+      await queryClient.cancelQueries({ queryKey: ['todayEmotionLog'] });
+
+      queryClient.setQueryData(['todayEmotionLog', userId], {
+        ...previousData,
+        emotion: params.emotion,
+      });
+      return previousData;
+    },
+    onError: (error, variables, context) => {
+      if (context) {
+        queryClient.setQueryData(['todayEmotionLog'], context);
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['todayEmotionLog'] });
+    },
+  });
+};
--- src/components/epigrams/DailyEmotion.tsx
+++ src/components/epigrams/DailyEmotion.tsx
@@ -3,20 +3,42 @@
 import { AnimatePresence, motion } from 'motion/react';
 import { EMOTION_STATUS, EMOTION_STATUS_KR } from '@/constants/emotions';
 import EmotionButton from '@/components/ui/emotionButton';
+import { useGetTodayEmotionLog, useOptimisticEmotionLog } from '@/apis/emotion-log/queries';
+import { Emotion } from '@/apis/emotion-log/types';
+import { useGetUser } from '@/apis/user/queries';
+import { useEffect } from 'react';
 
 export default function DailyEmotion() {
+  const { mutateAsync: updateEmotionLog } = useOptimisticEmotionLog();
+  const { data: user } = useGetUser();
+  const { data: dailyEmotion, refetch, isFetching } = useGetTodayEmotionLog(user?.id ?? 0);
+
+  useEffect(() => {
+    if (user) {
+      refetch();
+    }
+  }, [user, refetch]);
+
+  const handleEmotionClick = (emotion: Emotion) => {
+    if (user) updateEmotionLog({ params: { emotion }, userId: user.id });
+  };
+
   return (
     <section className='flex flex-col gap-4 md:gap-8'>
-      <h2 className='text-black-600 text-lg font-semibold md:text-2xl'>오늘의 감정은 어떤가요?</h2>
       <AnimatePresence>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className='flex justify-center gap-4'>
-          {EMOTION_STATUS.map((emotion, index) => (
-            <motion.div key={index} animate={{ opacity: 1 }} transition={{ duration: 0.2 }} whileHover={{ y: -8, transition: { duration: 0.2 } }} className='flex flex-col items-center gap-2'>
-              <EmotionButton emotion={emotion} isInteractive />
-              <span className='text-sub-blue-1 lg:text-2lg text-sm font-semibold md:text-lg'>{EMOTION_STATUS_KR[index]}</span>
-            </motion.div>
-          ))}
-        </motion.div>
+        {user && !isFetching && !dailyEmotion && (
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0, transition: { duration: 1 } }} className='flex flex-col items-center gap-4'>
+            <h2 className='text-black-600 text-lg font-semibold md:text-2xl'>오늘의 감정은 어떤가요?</h2>
+            <div className='flex justify-center gap-4'>
+              {EMOTION_STATUS.map((emotion, index) => (
+                <motion.div key={index} animate={{ opacity: 1 }} transition={{ duration: 0.2 }} whileHover={{ y: -8, transition: { duration: 0.2 } }} className='flex flex-col items-center gap-2'>
+                  <EmotionButton emotion={emotion} isInteractive onClick={() => handleEmotionClick(emotion)} />
+                  <span className='text-sub-blue-1 lg:text-2lg text-sm font-semibold md:text-lg'>{EMOTION_STATUS_KR[index]}</span>
+                </motion.div>
+              ))}
+            </div>
+          </motion.div>
+        )}
       </AnimatePresence>
     </section>
   );
