2d5062356aa6b3fcc852acb6c12a070bd7514d5a
송형진 <sori4606@naver.com>
2025-03-21 17:04:10 +0900

    :truck: chore : 파일 이름 변경

--- src/components/form/EditEpigramForm.tsx
+++
@@ -1,9 +0,0 @@
-import MainHeader from '../ui/header/MainHeader';
-
-export default function EditEpigramForm() {
-  return (
-    <div className='flex flex-col'>
-      <MainHeader />
-    </div>
-  );
-}
