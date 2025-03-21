14a8a3478f2f908f6f5578345b9ac48ae2fa647d
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-03-21 16:00:44 +0900

    :recycle: refactor: Comment 컴포넌트 시간, 이미지 타입 수정

--- src/components/ui/comment/index.tsx
+++ src/components/ui/comment/index.tsx
@@ -1,62 +1,39 @@
-import React from "react";
-import Avatar from "@/components/ui/avatars";
+import React from 'react';
+import Avatar from '@/components/ui/avatars';
 
 interface CommentProps {
   isOwnComment: boolean;
   nickname: string;
-  commentTime: string;
+  commentTime: string | null;
   content: string;
-  profileImage: string;
+  profileImage: string | null;
   onEdit?: () => void;
   onDelete?: () => void;
 }
 
-export default function Comment({
-  isOwnComment,
-  nickname,
-  commentTime,
-  content,
-  profileImage,
-  onEdit,
-  onDelete,
-  ...props
-}: CommentProps) {
+export default function Comment({ isOwnComment, nickname, commentTime, content, profileImage, onEdit, onDelete, ...props }: CommentProps) {
   return (
-    <div
-      className="flex max-w-[640px] bg-background-100 hover:shadow-lg transition-all duration-300 ease-in-out border-t border-line-200 py-4 sm:py-4 md:py-6 lg:py-8 px-6"
-      {...props}
-    >
+    <div className='bg-background-100 border-line-200 flex max-w-[640px] border-t px-6 py-4 transition-all duration-300 ease-in-out hover:shadow-lg sm:py-4 md:py-6 lg:py-8' {...props}>
       {/* 왼쪽: 이미지 박스 */}
-      <div className="w-12 h-12 mr-4 rounded-full overflow-hidden">
-        <Avatar src={profileImage} alt={`${nickname}님의 프로필 이미지`}
-          size="default" />
+      <div className='mr-4 h-12 w-12 overflow-hidden rounded-full'>
+        <Avatar src={profileImage} alt={`${nickname}님의 프로필 이미지`} size='default' />
       </div>
 
       {/* 오른쪽: 텍스트 박스 */}
-      <div className="flex-1 flex flex-col">
+      <div className='flex flex-1 flex-col'>
         {/* 상단 박스: 닉네임, 댓글 시간, 수정/삭제 버튼 */}
-        <div className="flex justify-between items-center mb-2">
-          <div className="flex items-center">
-            <span className="mr-2 text-black-300 text-xs sm:text-xs md:text-md lg:text-lg">
-              {nickname}
-            </span>
-            <span className="text-black-300 text-xs sm:text-xs md:text-md lg:text-lg">
-              {commentTime}
-            </span>
+        <div className='mb-2 flex items-center justify-between'>
+          <div className='flex items-center'>
+            <span className='text-black-300 md:text-md mr-2 text-xs sm:text-xs lg:text-lg'>{nickname}</span>
+            <span className='text-black-300 md:text-md text-xs sm:text-xs lg:text-lg'>{commentTime}</span>
           </div>
 
           {isOwnComment && (
-            <div className="flex space-x-4">
-              <button
-                className="cursor-pointer text-black-600 underline text-xs sm:text-xs md:text-md lg:text-lg"
-                onClick={onEdit}
-              >
+            <div className='flex space-x-4'>
+              <button className='text-black-600 md:text-md cursor-pointer text-xs underline sm:text-xs lg:text-lg' onClick={onEdit}>
                 수정
               </button>
-              <button
-                className="cursor-pointer text-error underline text-xs sm:text-xs md:text-md lg:text-lg"
-                onClick={onDelete}
-              >
+              <button className='text-error md:text-md cursor-pointer text-xs underline sm:text-xs lg:text-lg' onClick={onDelete}>
                 삭제
               </button>
             </div>
@@ -64,9 +41,7 @@ export default function Comment({
         </div>
 
         {/* 댓글 내용 */}
-        <div className="text-black-700 text-md sm:text-md md:text-lg lg:text-xl">
-          {content}
-        </div>
+        <div className='text-black-700 text-md sm:text-md md:text-lg lg:text-xl'>{content}</div>
       </div>
     </div>
   );
