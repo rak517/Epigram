dcbbbb96f64afc2ddbb380c44d66f79da7efbf92
jaehyeongjung <titochung625@naver.com>
2025-03-21 13:51:39 +0900

    :recycle: refactor: variant 추가 및 필요없는 값 삭제

--- src/components/ui/buttons/roundedButton.tsx
+++ src/components/ui/buttons/roundedButton.tsx
@@ -4,21 +4,29 @@ import { cva, VariantProps } from 'class-variance-authority';
 
 const buttonVariants = cva(
   cn(
-    'flex items-center justify-center rounded-full border whitespace-nowrap transition hover:scale-105 hover:opacity-100',
+    'flex items-center justify-center rounded-full whitespace-nowrap transition hover:scale-105 hover:opacity-100',
     'cursor-pointer',
-    'disabled:cursor-not-allowed disabled:opacity-50',
-    'bg-transparent',
-    'border-transparent',
+    'disabled:cursor-not-allowed disabled:opacity-80 disabled:hover:scale-100',
+    'text-md lg:text-xl',
   ),
   {
     variants: {
       size: {
-        sm: 'min-w-[76px] h-9 px-3 py-1', 
-        md: 'min-w-[102px] h-12 px-4 py-2',
+        sm: 'px-4 py-2',
+        md: 'px-6 py-2',
+        lg: 'px-8 py-4',
+        xl: 'px-10 py-5',
+      },
+      variant: {
+        default: 'bg-line-100 text-gray-300',
+        secondary: 'bg-black-600 text-blue-100',
+        tertiary: 'bg-blue-900 text-blue-100',
+        outline: 'border border-line-200 bg-transparent text-blue-500 font-medium',
       },
     },
     defaultVariants: {
-      size: 'md', // 디폴트 값 
+      size: 'md', // 디폴트 값
+      variant: 'default',
     },
   },
 );
@@ -28,9 +36,9 @@ export interface RoundedButtonProps extends ButtonHTMLAttributes<HTMLButtonEleme
   children?: ReactNode;
 }
 
-function RoundedButton({ size, className, children, ...props }: RoundedButtonProps) {
+function RoundedButton({ size, variant, className, children, ...props }: RoundedButtonProps) {
   return (
-    <button className={cn(buttonVariants({ size, className }))} {...props}>
+    <button className={cn(buttonVariants({ size, variant, className }))} {...props}>
       {children}
     </button>
   );
--- src/stories/buttons/roundedButton.stories.tsx
+++ src/stories/buttons/roundedButton.stories.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import RoundedButton, { RoundedButtonProps } from '@/components/ui/buttons/roundedButton';
-import Image from 'next/image';
-import plus from '@/assets/icons/plus.svg';
 
 export default {
   title: 'Button/RoundedButton',
@@ -10,38 +8,69 @@ export default {
   argTypes: {
     size: {
       control: 'select',
-      options: ['sm', 'md'], 
+      options: ['sm', 'md', 'lg', 'xl'],
     },
-    disabled: { control: 'boolean' },
+    variant: {
+      control: 'select',
+      options: ['default', 'secondary', 'tertiary', 'outline'],
+    },
     className: { control: 'text' },
+    disabled: { control: 'boolean' },
   },
 } as Meta<typeof RoundedButton>;
 
-
 const Template: StoryFn<RoundedButtonProps> = (args) => <RoundedButton {...args} />;
 
-
+// 왕도로 가는길은 Default 사용
 export const Default = Template.bind({});
 Default.args = {
   size: 'md',
+  variant: 'default',
   children: '기본 버튼',
 };
 
+// 좋아요 버튼은 secondary 사용
+export const Secondary = Template.bind({});
+Secondary.args = {
+  size: 'md',
+  variant: 'secondary',
+  children: 'Secondary 버튼',
+};
+
+// 에픽그램 만들기 버튼은 tertiary
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  size: 'md',
+  variant: 'tertiary',
+  children: 'Tertiary 버튼',
+};
+
+// 더보기, 에픽그램 및 댓글추가 Outline
+export const Outline = Template.bind({});
+Outline.args = {
+  size: 'md',
+  variant: 'outline',
+  children: 'Outline 버튼',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'lg',
+  variant: 'default',
+  children: 'Large 버튼',
+};
+
+export const ExtraLarge = Template.bind({});
+ExtraLarge.args = {
+  size: 'xl',
+  variant: 'default',
+  children: 'XL 버튼',
+};
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   size: 'md',
+  variant: 'default',
   disabled: true,
   children: '비활성화 버튼',
-};
-
-export const WithIcon = Template.bind({});
-WithIcon.args = {
-  size: 'md',
-  children: (
-    <>
-      <Image src={plus} alt="더하기 아이콘" width={20} height={20} />
-      <span>추가</span> 
-    </>
-  ),
 };
