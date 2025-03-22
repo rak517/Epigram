f96ac5a73b56bc4a4372ace818e303e01db764c5
송형진 <sori4606@naver.com>
2025-03-22 15:28:57 +0900

    :wrench: chore : edit 파일 삭제 및 구조변경

---
+++ src/components/form/AddEpigramForm.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import MainHeader from '../ui/header/MainHeader';
+
+export default function AddEpigramForm() {
+  return (
+    <div className='flex flex-col'>
+      <MainHeader />
+    </div>
+  );
+}
