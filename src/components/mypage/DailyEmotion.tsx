f49ff847b753eb2b3d914fface6f57e7f2db1f9d
Kim To Kyun <58878190+ToKyun02@users.noreply.github.com>
2025-03-24 20:03:11 +0900

    ✨ feat : 오늘의 감정 컴포넌트(마이페이지) (#118)
    * :recycle: refactor : 메인페이지 감정 컴포넌트 disable variant 적용
    
    * :recycle: refactor : emotion button click 상태 로직 수정
    
    * :sparkles: feat : 오늘의 감정 컴포넌트(마이페이지) 구현

--- src/components/mypage/DailyEmotion.tsx
+++ src/components/mypage/DailyEmotion.tsx
@@ -5,35 +5,43 @@ import { EMOTION_STATUS, EMOTION_STATUS_KR } from '@/constants/emotions';
 import EmotionButton from '@/components/ui/emotionButton';
 import { useGetTodayEmotionLog, useOptimisticEmotionLog } from '@/apis/emotion-log/queries';
 import { Emotion } from '@/apis/emotion-log/types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
 
 export default function DailyEmotion() {
   const { mutateAsync: updateEmotionLog } = useOptimisticEmotionLog();
-  const { data: dailyEmotion, isFetching } = useGetTodayEmotionLog();
-  const [isPosting, setIsPosting] = useState(false);
+  const { data: dailyEmotion } = useGetTodayEmotionLog();
+  const [date, setDate] = useState<string | null>(null);
 
   const handleEmotionClick = async (emotion: Emotion) => {
-    setIsPosting(true);
     await updateEmotionLog({ emotion });
-    setIsPosting(false);
   };
+
+  useEffect(() => {
+    const today = new Date();
+    const formattedDate = dayjs(today).format('YYYY.MM.DD');
+    setDate(formattedDate);
+  }, []);
 
   return (
     <section className='flex flex-col gap-4 md:gap-8'>
       <AnimatePresence>
-        {!isFetching && !dailyEmotion && (
+        {
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0, transition: { duration: 1 } }} className='flex flex-col items-center gap-4'>
-            <h2 className='text-black-600 text-lg font-semibold md:text-2xl'>오늘의 감정은 어떤가요?</h2>
+            <div className='flex w-full items-center justify-between'>
+              <h2 className='text-black-600 text-lg font-semibold md:text-2xl'>오늘의 감정</h2>
+              <span className='text-lg text-blue-400 lg:text-xl'>{date}</span>
+            </div>
             <div className='flex justify-center gap-4'>
               {EMOTION_STATUS.map((emotion, index) => (
                 <motion.div key={index} animate={{ opacity: 1 }} transition={{ duration: 0.2 }} whileHover={{ y: -8, transition: { duration: 0.2 } }} className='flex flex-col items-center gap-2'>
-                  <EmotionButton emotion={emotion} isInteractive onClick={() => handleEmotionClick(emotion)} disabled={isPosting} emotionVariant={isPosting ? 'grayScales' : 'default'} />
+                  <EmotionButton emotion={emotion} isInteractive onClick={() => handleEmotionClick(emotion)} buttonVariant={dailyEmotion?.emotion === emotion} />
                   <span className='text-sub-blue-1 lg:text-2lg text-sm font-semibold md:text-lg'>{EMOTION_STATUS_KR[index]}</span>
                 </motion.div>
               ))}
             </div>
           </motion.div>
-        )}
+        }
       </AnimatePresence>
     </section>
   );
