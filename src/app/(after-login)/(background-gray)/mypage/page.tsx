04edcbf63ef111ff665aeb8be923b15176fbcd0f
CJewon <112820744+CJewon@users.noreply.github.com>
2025-03-26 09:56:23 +0900

    :art: art : 마이페이지 테스트코드 삭제

--- src/app/(after-login)/(background-gray)/mypage/page.tsx
+++ src/app/(after-login)/(background-gray)/mypage/page.tsx
@@ -1,9 +1,3 @@
-import EmotionChart from '@/components/epigrams/EmotionChart';
-
 export default function MyPage() {
-  return (
-    <div className='flex items-center justify-center'>
-      <EmotionChart></EmotionChart>
-    </div>
-  );
+  return <div>마이페이지</div>;
 }
