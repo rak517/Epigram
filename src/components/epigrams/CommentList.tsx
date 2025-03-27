f7c9e503ff84c58deb8b74afde72c3437c50907f
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-03-27 14:43:41 +0900

    :recycle: refactor: 댓글 렌더링 상태 early return pattern으로 변경
    - 모달 로직 커스텀 훅 사용

--- src/components/epigrams/CommentList.tsx
+++ src/components/epigrams/CommentList.tsx
@@ -1,29 +1,24 @@
 'use client';
 
-import { useCommentsQuery, useDeleteComment, useUpdateComment } from '@/apis/comment/queries';
-import Comment from '../ui/comment';
-import RoundedButton from '../ui/buttons/roundedButton';
-import getTimeElapsed from '@/utils/getTimeElapsed';
-import { useModalStore } from '@/stores/ModalStore';
-import EditCommentModal from './EditCommentModal';
+import { useMemo } from 'react';
+import { useCommentsQuery } from '@/apis/comment/queries';
 import { useGetUser } from '@/apis/user/queries';
-import { getErrorMessage } from '@/utils/network/getErrorMessage';
-import { Comment as CommentType, PatchComment } from '@/apis/comment/types';
+import { useCommentActions } from '@/hooks/useCommentActions';
+import getTimeElapsed from '@/utils/getTimeElapsed';
+import Comment from '../ui/comment';
 import CommentsSkeleton from '../ui/skeletons/CommentLIstSkeleton';
-import plus_ic from '@/assets/icons/plus.svg';
+import RoundedButton from '../ui/buttons/roundedButton';
 import Image from 'next/image';
+import plus_ic from '@/assets/icons/plus.svg';
+import CommentEmptyState from './CommentEmptyState';
 
 const PAGE_LIMIT = 4;
 
 export default function CommentList() {
   const { data, isLoading, hasNextPage, fetchNextPage, isFetchingNextPage } = useCommentsQuery(PAGE_LIMIT);
-  const comments = data?.pages.flatMap((page) => page.list) ?? [];
-  const updateComment = useUpdateComment();
-  const deleteComment = useDeleteComment();
-
+  const comments = useMemo(() => data?.pages.flatMap((page) => page.list) ?? [], [data]);
   const user = useGetUser();
-
-  const { openModal, closeAllModals } = useModalStore();
+  const { handleEditComment, handleDelete } = useCommentActions();
 
   const handleMoreClick = () => {
     if (hasNextPage) {
@@ -31,84 +26,42 @@ export default function CommentList() {
     }
   };
 
-  const handleEditComment = (comment: CommentType) => {
-    openModal({
-      type: 'custom',
-      content: (
-        <EditCommentModal
-          initialContent={comment.content}
-          commentId={comment.id}
-          onSave={async (data: { commentId: number; patchData: PatchComment }) => {
-            try {
-              await updateComment.mutateAsync(data);
-              openModal({
-                type: 'alert',
-                title: '수정 성공',
-                description: '댓글 수정에 성공했습니다.',
-                callback: closeAllModals,
-              });
-            } catch (err) {
-              openModal({
-                type: 'alert',
-                title: '댓글 수정 실패',
-                callback: () => getErrorMessage(err),
-              });
-            }
-          }}
-        />
-      ),
-      size: 'md',
-    });
-  };
+  const renderContent = () => {
+    if (isLoading) {
+      return <CommentsSkeleton count={PAGE_LIMIT} />;
+    }
 
-  const handleDelete = (commentId: number) => {
-    openModal({
-      type: 'confirm',
-      title: '정말 삭제하시겠습니까?',
-      callback: async () => {
-        try {
-          await deleteComment.mutateAsync(commentId);
-          openModal({
-            type: 'alert',
-            title: '삭제 성공',
-            description: '댓글을 삭제했습니다.',
-            callback: closeAllModals,
-          });
-        } catch (err) {
-          openModal({
-            type: 'alert',
-            title: '삭제실패',
-            callback: () => getErrorMessage(err),
-          });
-        }
-      },
-    });
+    if (comments.length === 0) {
+      return <CommentEmptyState />;
+    }
+
+    return (
+      <div className='w-full'>
+        {comments.map((comment) => {
+          const isOwnComment = user.data?.id === comment.writer.id;
+          return (
+            <Comment
+              nickname={comment.writer.nickname}
+              commentTime={getTimeElapsed(comment.updatedAt)}
+              content={comment.content}
+              profileImage={comment.writer.image}
+              isOwnComment={isOwnComment}
+              onEdit={isOwnComment ? () => handleEditComment(comment) : undefined}
+              onDelete={() => handleDelete(comment.id)}
+              key={comment.id}
+            />
+          );
+        })}
+      </div>
+    );
   };
 
   return (
     <section className='flex w-full flex-col gap-4 lg:gap-10'>
       <h3 className='text-black-600 pl-6 text-lg font-semibold md:pl-0 lg:text-2xl'>최신 댓글</h3>
-      {isLoading ? (
-        <CommentsSkeleton count={PAGE_LIMIT} />
-      ) : (
-        <div className='w-full'>
-          {comments.map((comment) => {
-            const isOwnComment = user.data?.id === comment.writer.id;
-            return (
-              <Comment
-                nickname={comment.writer.nickname}
-                commentTime={getTimeElapsed(comment.updatedAt)}
-                content={comment.content}
-                profileImage={comment.writer.image}
-                isOwnComment={isOwnComment}
-                onEdit={isOwnComment ? () => handleEditComment(comment) : undefined}
-                onDelete={() => handleDelete(comment.id)}
-                key={comment.id}
-              />
-            );
-          })}
-        </div>
-      )}
+
+      {renderContent()}
+
       {isFetchingNextPage && <CommentsSkeleton count={PAGE_LIMIT} />}
       <div className='flex justify-center'>
         {hasNextPage && (
