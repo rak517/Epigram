3df995166f3c8c137c79f9c155d95ae4715d994a
CJewon <112820744+CJewon@users.noreply.github.com>
2025-04-01 19:55:15 +0900

    :wrench: fixed : 중복된 flatMap 호출 제거 및 변수 선언으로 코드 개선

--- src/components/mypage/MyEpigramComments.tsx
+++ src/components/mypage/MyEpigramComments.tsx
@@ -18,7 +18,7 @@ export default function MyEpigramComments() {
   const { data: userdata } = useGetUser();
   const userId = userdata?.id as number;
 
-  const { data: myEpigrams, fetchNextPage: fetchNextEpigrams, hasNextPage: hasNextEpigrams, isFetchingNextPage: isFetchingEpigrams } = useGetEpigrams({ limit: 3, writerId: userId });
+  const { data: myEpigramsData, fetchNextPage: fetchNextEpigrams, hasNextPage: hasNextEpigrams, isFetchingNextPage: isFetchingEpigrams } = useGetEpigrams({ limit: 3, writerId: userId });
 
   const handleLoadEpigramsButton = () => {
     if (hasNextEpigrams) {
@@ -26,7 +26,13 @@ export default function MyEpigramComments() {
     }
   };
 
-  const { data: myComments, fetchNextPage: fetchNextComments, hasNextPage: hasNextComments, isFetchingNextPage: isFetchingComments } = useGetComments(userId, { limit: 4 });
+  const { data: myCommentsData, fetchNextPage: fetchNextComments, hasNextPage: hasNextComments, isFetchingNextPage: isFetchingComments } = useGetComments(userId, { limit: 4 });
+
+  const myCommentTotalCount = myCommentsData?.pages[0].totalCount;
+  const myEpigramTotalCount = myEpigramsData?.pages[0].totalCount;
+
+  const myEpigrams = myEpigramsData?.pages.flatMap((page) => page.list) ?? [];
+  const myComments = myCommentsData?.pages.flatMap((page) => page.list) ?? [];
 
   const handleLoadCommentsButton = () => {
     if (hasNextComments) {
@@ -36,19 +42,19 @@ export default function MyEpigramComments() {
 
   return (
     <div className='flex flex-col gap-6 md:gap-8 lg:gap-12'>
-      <div className='flex gap-4 px-6 lg:gap-12'>
+      <div className='flex gap-4 lg:gap-12'>
         <button
           onClick={() => setButtonTab('myEpigram')}
           className={`text-lg font-semibold lg:text-2xl ${buttonTab === 'myEpigram' ? 'text-black-600' : 'text-gray-300'}`}
-        >{`내 에피그램(${myEpigrams?.pages[0].totalCount ?? 0})`}</button>
+        >{`내 에피그램(${myEpigramTotalCount ?? 0})`}</button>
         <button
           onClick={() => setButtonTab('myComment')}
           className={`text-lg font-semibold lg:text-2xl ${buttonTab === 'myComment' ? 'text-black-600' : 'text-gray-300'}`}
-        >{`내 댓글(${myComments?.pages[0].totalCount ?? 0})`}</button>
+        >{`내 댓글(${myCommentTotalCount ?? 0})`}</button>
       </div>
 
-      {buttonTab === 'myEpigram' && myEpigrams ? (
-        myEpigrams.pages.flatMap((page) => page.list).length === 0 ? (
+      {buttonTab === 'myEpigram' && myEpigramsData ? (
+        myEpigrams.length === 0 ? (
           <div className='flex flex-col items-center justify-center gap-12 md:gap-16 lg:gap-20'>
             <Image className='pb-2 md:pb-4 lg:pb-6' src={EmptyIcon} alt='내 에피그램이 없습니다.' />
             <p className='text-black-600 pb-8 text-center text-[14px] font-normal md:pb-10 lg:pb-12 lg:text-[20px]'>
@@ -62,21 +68,19 @@ export default function MyEpigramComments() {
           </div>
         ) : (
           <div className='flex flex-col items-center justify-center gap-12 md:gap-16 lg:gap-20'>
-            {myEpigrams.pages.flatMap((page) =>
-              page.list.map((epigram) => (
-                <Link key={epigram.id} href={`/epigrams/${epigram.id}`}>
-                  <TextCard
-                    author={epigram.author}
-                    cardContent={epigram.content}
-                    tags={epigram.tags}
-                    tagPosition='bottomRight'
-                    tagClassName='text-[14px] md:text-lg'
-                    authorClassName='text-[14px] md:text-lg'
-                    className='w-[312px] md:w-[384px] lg:w-[640px]'
-                  />
-                </Link>
-              )),
-            )}
+            {myEpigrams.map((epigram) => (
+              <Link key={epigram.id} href={`/epigrams/${epigram.id}`}>
+                <TextCard
+                  author={epigram.author}
+                  cardContent={epigram.content}
+                  tags={epigram.tags}
+                  tagPosition='bottomRight'
+                  tagClassName='text-[14px] md:text-lg'
+                  authorClassName='text-[14px] md:text-lg'
+                  className='w-[312px] md:w-[384px] lg:w-[640px]'
+                />
+              </Link>
+            ))}
             {hasNextEpigrams && (
               <RoundedButton
                 className='bg-background-100 border-line-200 flex h-12 w-[153px] gap-1 border px-[18px] py-3 text-[14px] font-medium text-blue-500 lg:h-14 lg:w-[238px] lg:text-xl'
@@ -95,8 +99,8 @@ export default function MyEpigramComments() {
             )}
           </div>
         )
-      ) : buttonTab === 'myComment' && myComments ? (
-        myComments.pages.flatMap((page) => page.list).length === 0 ? (
+      ) : buttonTab === 'myComment' && myCommentsData ? (
+        myComments.length === 0 ? (
           <div className='flex h-[304px] flex-col items-center justify-center lg:h-[488px]'>
             <Image className='pb-2 md:pb-4 lg:pb-6' src={EmptyIcon} alt='내 댓글이 없습니다.' />
             <p className='text-black-600 pb-8 text-center text-[14px] font-normal md:pb-10 lg:pb-12 lg:text-[20px]'>
@@ -109,14 +113,12 @@ export default function MyEpigramComments() {
             </Link>
           </div>
         ) : (
-          <div className=''>
-            {myComments.pages.flatMap((page) =>
-              page.list.map((comment) => (
-                <Link key={comment.id} href={`/epigrams/${comment.epigramId}`}>
-                  <Comment nickname={comment.writer.nickname} isOwnComment={true} content={comment.content} commentTime={getTimeElapsed(comment.createdAt)} profileImage={comment.writer.image} />
-                </Link>
-              )),
-            )}
+          <div>
+            {myComments.map((comment) => (
+              <Link key={comment.id} href={`/epigrams/${comment.epigramId}`}>
+                <Comment nickname={comment.writer.nickname} isOwnComment={true} content={comment.content} commentTime={getTimeElapsed(comment.createdAt)} profileImage={comment.writer.image} />
+              </Link>
+            ))}
             {hasNextComments && (
               <RoundedButton
                 className='bg-background-100 border-line-200 mx-auto mt-14 flex h-12 w-[153px] gap-1 border px-[18px] py-3 text-[14px] font-medium text-blue-500 md:mt-10 lg:mt-18 lg:h-14 lg:w-[238px] lg:text-xl'
