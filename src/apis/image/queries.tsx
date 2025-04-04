a3572f5f8729ad83a0f79c04fa4b77d5523e7082
CJewon <112820744+CJewon@users.noreply.github.com>
2025-04-04 14:21:49 +0900

    :sparkles: feat : 프로필 이미지 클릭 시, 프로필 이미지를 변경하는 기능 구현

--- src/apis/image/queries.tsx
+++ src/apis/image/queries.tsx
@@ -1,10 +1,21 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postImage } from '.';
+import { patchUser } from '../user';
 
 export const usePostImage = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
-    mutationFn: (form: FormData) => {
-      return postImage(form);
+    mutationFn: async (form: FormData) => {
+      const uploadResponse = await postImage(form);
+      const imageUrl = uploadResponse.url;
+
+      const patchResponse = await patchUser({ image: imageUrl });
+
+      return patchResponse;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] });
     },
   });
 };
--- src/app/(after-login)/(background-gray)/mypage/page.tsx
+++ src/app/(after-login)/(background-gray)/mypage/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { usePostImage } from '@/apis/image/queries';
 import LogoutButton from '@/components/auth/LogoutButton';
 import DailyEmotion from '@/components/mypage/DailyEmotion';
 import EmotionCalendar from '@/components/mypage/EmotionCalendar';
@@ -7,16 +8,33 @@ import EmotionChart from '@/components/mypage/EmotionChart';
 import MyEpigramComments from '@/components/mypage/MyEpigramComments';
 import Avatar from '@/components/ui/avatars';
 import { MypageContext } from '@/context/MypageProvider';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 
 export default function MyPage() {
   const { user } = useContext(MypageContext);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const { mutate: upLoadImage } = usePostImage();
+
+  const handleAvatarClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append('image', file);
+
+    upLoadImage(formData);
+  };
 
   return (
     <div className='bg-background-100'>
       <div className='shadow-custom relative mt-16 flex flex-col items-center justify-center rounded-3xl bg-blue-100 px-6 pt-[184px] pb-10 shadow-black md:pb-[63px] lg:pt-[276px] lg:pb-22'>
         <div className='absolute top-0 left-1/2 flex -translate-x-1/2 -translate-y-[40px] flex-col items-center justify-center gap-4 lg:-translate-y-[60px] lg:gap-6'>
-          <Avatar src={user.image} alt='유저이미지' className='size-20 border-2 border-blue-300 lg:size-[120px]'></Avatar>
+          <Avatar src={user.image} alt='유저이미지' className='size-20 cursor-pointer border-2 border-blue-300 lg:size-[120px]' onClick={handleAvatarClick}></Avatar>
+          <input type='file' className='hidden' onChange={handleChangeImage} accept='image/*' ref={fileInputRef} />
           <p className='text-black-950 text-lg font-medium lg:text-2xl'>{user.nickname}</p>
           <LogoutButton className='h-9 w-[77px] px-3.5 py-1.5 text-[14px] font-normal whitespace-nowrap lg:h-12 lg:w-[100px] lg:px-4 lg:py-2 lg:text-xl lg:font-medium'></LogoutButton>
         </div>
