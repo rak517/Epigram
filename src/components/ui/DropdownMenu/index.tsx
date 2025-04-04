dc73ea1f2277da67a73e3964c3d335ef49ccd264
jaehyeongjung <titochung625@naver.com>
2025-04-04 13:54:14 +0900

    :recycle: refactor: DropdownMenu에 trigger prop 추가

--- src/components/ui/DropdownMenu/index.tsx
+++ src/components/ui/DropdownMenu/index.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import { useState } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/cn';
@@ -27,34 +27,22 @@ interface DropdownProps extends VariantProps<typeof DropdownVariants> {
   className?: string;
   options?: string[];
   onSelect?: (option: string) => void;
+  trigger?: React.ReactNode;
 }
 
-export default function DropdownMenu({
-  variant,
-  size = 'md',
-  options = ["수정하기", "삭제하기"],
-  onSelect,
-}: DropdownProps) {
+export default function DropdownMenu({ variant, size = 'md', options = ['수정하기', '삭제하기'], onSelect, trigger,  }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   const liPaddingClasses =
-    size === 'md'
-      ? 'py-2 text-md lg:py-4 lg:text-xl'
-      : size === 'sm'
-        ? 'py-2 text-xs lg:py-2 lg:text-md'
-        : size === 'lg'
-          ? 'py-2 text-md lg:py-6 lg:text-2xl'
-          : 'py-2 text-md lg:py-4 lg:text-xl';
+    size === 'md' ? 'py-2 text-md lg:py-4 lg:text-xl' : size === 'sm' ? 'py-2 text-xs lg:py-2 lg:text-md' : size === 'lg' ? 'py-2 text-md lg:py-6 lg:text-2xl' : 'py-2 text-md lg:py-4 lg:text-xl';
 
   return (
-    <div className="relative inline-block ">
-      <button onClick={() => setIsOpen(!isOpen)} className="cursor-pointer">
-        <Image src={dropdownIcon} alt="Dropdown Icon" />
+    <div className='relative inline-block'>
+      <button onClick={() => setIsOpen(!isOpen)} className='cursor-pointer'>
+        {trigger ?? <Image src={dropdownIcon} alt='Dropdown Icon' />}
       </button>
       {isOpen && (
-        <ul className={cn(DropdownVariants({ variant, size }),
-          'top-[45px] right-0'
-        )}>
+        <ul className={cn(DropdownVariants({ variant, size }), 'top-[45px] right-0')}>
           {options.map((option) => (
             <li
               key={option}
@@ -62,11 +50,7 @@ export default function DropdownMenu({
                 setIsOpen(false);
                 onSelect?.(option);
               }}
-              className={cn(
-                liPaddingClasses,
-                'hover:bg-gray-100 cursor-pointer text-center',
-                'text-[var(--color-black-800)]'
-              )}
+              className={cn(liPaddingClasses, 'cursor-pointer text-center hover:bg-gray-100', 'text-[var(--color-black-800)]')}
             >
               {option}
             </li>
--- src/components/ui/header/LandingHeader.tsx
+++ src/components/ui/header/LandingHeader.tsx
@@ -1,34 +1,51 @@
 'use client';
+
 import Image from 'next/image';
 import headerLogo from '@/assets/images/headerLogo.svg';
 import userIconDark from '@/assets/icons/userIconDark.svg';
 import searchIcon from '@/assets/icons/searchIcon.svg';
 import Link from 'next/link';
+import DropdownMenu from '@/components/ui/DropdownMenu';
+import { useRouter } from 'next/navigation';
 
 export interface LandingHeaderProps {
   showIcon?: boolean;
 }
 
 export default function LandingHeader({ showIcon = false }: LandingHeaderProps) {
+  const router = useRouter();
+
+  const handleSelect = (option: string) => {
+    if (option === '마이페이지') {
+      router.push('/mypage');
+    } else if (option === '로그아웃') {
+      console.log('로그아웃');
+    }
+  };
+
   if (showIcon) {
     return (
-      <header className='border-b-line-100 fixed z-100 flex w-full items-center justify-between border-b bg-white px-6 py-2 md:h-[60px] md:px-[72px] md:py-3 xl:h-20 xl:px-[120px] xl:py-4'>
+      <header className="border-b-line-100 fixed z-100 flex w-full items-center justify-between border-b bg-white px-6 py-2 md:h-[60px] md:px-[72px] md:py-3 xl:h-20 xl:px-[120px] xl:py-4">
         <Link href={'/search'}>
-          <Image src={searchIcon} alt='검색 아이콘' className='xl:w-9' />
+          <Image src={searchIcon} alt="검색 아이콘" className="xl:w-9" />
         </Link>
         <Link href={'/'}>
-          <Image src={headerLogo} alt='헤더 로고' className='h-[36px] w-[119px] xl:h-12 xl:w-[172px]' priority />
+          <Image src={headerLogo} alt="헤더 로고" className="h-[36px] w-[119px] xl:h-12 xl:w-[172px]" priority />
         </Link>
-        <Link href={'/mypage'}>
-          <Image src={userIconDark} alt='유저 아이콘' />
-        </Link>
+        <DropdownMenu
+          size="sm"
+          options={['마이페이지', '로그아웃']}
+          onSelect={handleSelect}
+          trigger={<Image src={userIconDark} alt="유저 아이콘" />}
+        />
       </header>
     );
   }
+
   return (
-    <header className='border-b-line-100 fixed z-100 flex w-full items-center justify-center border-b bg-white py-3.5 md:h-[60px] xl:h-20'>
-      <Link href={'/'} className='flex items-center justify-center'>
-        <Image src={headerLogo} alt='헤더 로고' className='xl:h-9 xl:w-[131px]' />
+    <header className="border-b-line-100 fixed z-100 flex w-full items-center justify-center border-b bg-white py-3.5 md:h-[60px] xl:h-20">
+      <Link href={'/'} className="flex items-center justify-center">
+        <Image src={headerLogo} alt="헤더 로고" className="xl:h-9 xl:w-[131px]" />
       </Link>
     </header>
   );
