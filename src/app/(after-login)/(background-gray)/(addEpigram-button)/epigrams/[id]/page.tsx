dde32efd93b5fe59101eba0d9658faf58cf225b0
sori4606 <110970570+sori4606@users.noreply.github.com>
2025-04-11 11:03:49 +0900

    :recycle: refactor: 코드 서버컴포넌트 페칭 이전으로 롤백 및 lighthouse 개선사항 유지(#212)

--- src/app/(after-login)/(background-gray)/(addEpigram-button)/epigrams/[id]/page.tsx
+++ src/app/(after-login)/(background-gray)/(addEpigram-button)/epigrams/[id]/page.tsx
@@ -1,8 +1,5 @@
 import EpigramComment from '@/components/epigramDetail/EpigramComment';
 import EpigramContent from '@/components/epigramDetail/EpigramContent';
-import EpigramDropdown from '@/components/epigramDetail/EpigramDropdown';
-import TagsClient from '@/components/epigramDetail/TagsClient';
-import TextCardServer from '@/components/ui/textcard/textcardServer';
 import { Metadata } from 'next';
 
 export async function generateMetadata({ params }: { params: Promise<{ id: string }> }): Promise<Metadata> {
@@ -31,42 +28,10 @@ export async function generateMetadata({ params }: { params: Promise<{ id: strin
   };
 }
 
-const REVALIDATE_TIME = 60 * 10;
-
-export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/epigrams/${id}`, {
-    next: {
-      revalidate: REVALIDATE_TIME,
-    },
-  });
-
-  const data = await response.json();
-
+export default function Page() {
   return (
     <div className='-mt-6 w-full pt-0 md:-mt-8 lg:-mt-10'>
-      <div className='flex w-full flex-col items-center justify-center bg-[linear-gradient(white_90%,#f2f2f2)] bg-[length:100%_20px] bg-repeat pt-32 pb-1 shadow-[0px_3px_12px_0px_rgba(0,0,0,0.04)]'>
-        <div className='relative h-[164px] w-[312px] text-2xl md:h-[182px] md:w-[384px] lg:h-[236px] lg:w-[640px] lg:text-3xl'>
-          <TextCardServer
-            variant='variableHeight'
-            tagPosition='topLeft'
-            hasBackground={false}
-            hasBorder={false}
-            authorClassName='text-base md:text-xl lg:text-2xl'
-            cardContent={data?.content}
-            author={data?.author}
-            className='h-full w-full'
-          />
-
-          <TagsClient tags={data.tags} tagPosition='topLeft' tagClassName='text-base lg:text-xl pl-4' />
-          <EpigramDropdown writerId={data.writerId} epigramId={data.id} />
-        </div>
-
-        <EpigramContent epigramId={data.id} initialLikeCount={data.likeCount} initialIsLiked={data.isLiked ?? false} referenceUrl={data.referenceUrl} />
-
-        <div className='after:zigzag relative h-4 w-full'></div>
-      </div>
-
+      <EpigramContent />
       <EpigramComment />
     </div>
   );
--- src/components/epigramDetail/EpigramContent.tsx
+++ src/components/epigramDetail/EpigramContent.tsx
@@ -1,59 +1,60 @@
 'use client';
 
 import RoundedButton from '../ui/buttons/roundedButton';
+import TextCard from '../ui/textcard';
 import like from '@/assets/icons/like.svg';
 import like_outlined from '@/assets/icons/like_outlined.svg';
 import external from '@/assets/icons/external-link.svg';
 import share from '@/assets/icons/share.svg';
 import Image from 'next/image';
-import { useDeleteEpigramFavorite, useGetEpigram, usePostEpigramFavorite } from '@/apis/epigram/queries';
-import { useRouter } from 'next/navigation';
+import { useDeleteEpigram, useDeleteEpigramFavorite, useGetEpigram, usePostEpigramFavorite } from '@/apis/epigram/queries';
+import { useParams, useRouter } from 'next/navigation';
 import { useModalStore } from '@/stores/ModalStore';
 import { isAxiosError } from 'axios';
+import { useGetUser } from '@/apis/user/queries';
 import { getErrorMessage } from '@/utils/network/getErrorMessage';
 import { useToast } from '@/utils/toast/ToastContext';
 import { copyCurrentUrl } from '@/utils/copyCurrentUrl';
-import { useEffect, useState } from 'react';
 
-interface EpigramContentProps {
-  epigramId: number;
-  initialLikeCount: number;
-  initialIsLiked: boolean;
-  referenceUrl: string | null;
-}
-
-export default function EpigramContent({ epigramId, initialLikeCount, initialIsLiked, referenceUrl }: EpigramContentProps) {
-  const [likeCount, setLikeCount] = useState(initialLikeCount);
-  const [isLiked, setIsLiked] = useState(initialIsLiked);
-
+export default function EpigramContent() {
   const router = useRouter();
+  const params = useParams();
   const { openModal } = useModalStore();
-  const { showToast } = useToast();
+
+  const epigramId = params?.id ? Number(params.id) : undefined;
+
+  const { data, isError, isLoading, error } = useGetEpigram(epigramId);
+
+  const { data: user } = useGetUser();
+
+  const deleteEpigramMutation = useDeleteEpigram();
 
   const addFavoriteMutation = usePostEpigramFavorite();
+
   const deleteFavoriteMutation = useDeleteEpigramFavorite();
 
-  const { data } = useGetEpigram(epigramId);
+  const hasSourceUrl = data?.referenceUrl;
 
-  // 좋아요를 누르고 새로고침하고 다시 좋아요를 누르면 에러발생 (서버와 클라이언트 상태를 동기화하기 위해 작성)
-  useEffect(() => {
-    const verifyLikeStatus = async () => {
-      try {
-        if (data && data.isLiked !== undefined) {
-          const serverIsLiked = data?.isLiked;
+  const showDropdown = user?.id === data?.writerId;
 
-          if (serverIsLiked !== isLiked) {
-            setIsLiked(serverIsLiked);
-            setLikeCount(data?.likeCount);
-          }
-        }
-      } catch (error) {
-        console.error('좋아요 상태 오류', error);
-      }
-    };
-    verifyLikeStatus();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [epigramId]);
+  const { showToast } = useToast();
+
+  if (isLoading) {
+    return (
+      <div className='flex w-full flex-col items-center justify-center pt-18 pb-1'>
+        <div className='h-[164px] w-[312px] rounded-lg bg-gray-200 md:h-[182px] md:w-[384px] lg:h-[236px] lg:w-[640px]'></div>
+        <div className='px-auto flex w-full max-w-[640px] justify-center gap-4 pt-9'>
+          <div className='h-10 w-32 rounded-full bg-gray-200'></div>
+          <div className='h-10 w-36 rounded-full bg-gray-200'></div>
+          <div className='h-10 w-40 rounded-full bg-gray-200'></div>
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    throw error;
+  }
 
   const handleCopyUrl = () => {
     copyCurrentUrl(
@@ -62,30 +63,51 @@ export default function EpigramContent({ epigramId, initialLikeCount, initialIsL
     );
   };
 
+  const handleTagClick = (tagName: string) => {
+    router.push(`/search?keyword=${encodeURIComponent(tagName)}`);
+  };
+
+  const handleEdit = () => {
+    router.push(`/epigrams/${data?.id}/edit`);
+  };
+
+  const handleDeleteClick = () => {
+    openModal({
+      type: 'confirm',
+      title: '에피그램 삭제',
+      description: '정말로 이 에피그램을 삭제하시겠습니까?',
+      okMessage: '삭제',
+      cancelMessage: '취소',
+      callback: () => {
+        handleDelete();
+      },
+    });
+  };
+
+  const handleDelete = async () => {
+    if (data) {
+      deleteEpigramMutation.mutate(data.id, {
+        onError: () => {
+          showToast('에피그램 삭제 중 오류가 발생했습니다.', 'error', '삭제 실패');
+        },
+      });
+    }
+  };
+
   const handleToggleLike = async () => {
+    if (!data) return;
+
     try {
-      if (!isLiked) {
-        console.log('좋아요를 안누른상태에요');
-        await addFavoriteMutation.mutateAsync(epigramId);
-        setIsLiked(true);
-        setLikeCount((prev) => prev + 1);
+      if (!data.isLiked) {
+        await addFavoriteMutation.mutateAsync(data.id);
       } else {
-        console.log('좋아요를 누른상태에요');
-        await deleteFavoriteMutation.mutateAsync(epigramId);
-        setIsLiked(false);
-        setLikeCount((prev) => prev - 1);
+        await deleteFavoriteMutation.mutateAsync(data.id);
       }
     } catch (err) {
       let errorMessage = getErrorMessage(err);
       let callback = () => {};
 
       console.error('좋아요 처리 오류:', err);
-
-      if (isAxiosError(err) && err.response?.data?.message === '이미 좋아요를 눌렀습니다.') {
-        setIsLiked(true);
-        return;
-      }
-
       //세션 만료 처리
       if (isAxiosError(err) && err.status === 401) {
         errorMessage = '세션이 만료되었습니다.';
@@ -104,57 +126,76 @@ export default function EpigramContent({ epigramId, initialLikeCount, initialIsL
   };
 
   return (
-    <div className='px-auto flex gap-4 pt-9'>
-      {isLiked ? (
-        <RoundedButton variant='secondary' onClick={handleToggleLike}>
-          <div className='flex items-center justify-center'>
-            <div className='hidden md:block'>
-              <Image src={like} alt='좋아요 따봉 이미지' width={36} height={36} priority />
+    <div className='flex w-full flex-col items-center justify-center bg-[linear-gradient(white_90%,#f2f2f2)] bg-[length:100%_20px] bg-repeat pt-32 pb-1 shadow-[0px_3px_12px_0px_rgba(0,0,0,0.04)]'>
+      <TextCard
+        variant='variableHeight'
+        tagPosition='topLeft'
+        hasBackground={false}
+        isDropdown={showDropdown}
+        hasBorder={false}
+        tagClassName='text-base lg:text-xl pl-4'
+        authorClassName='text-base md:text-xl lg:text-2xl'
+        cardContent={data?.content}
+        author={data?.author}
+        tags={data?.tags}
+        onTagClick={handleTagClick}
+        onEdit={handleEdit}
+        onDelete={handleDeleteClick}
+        className='h-[164px] w-[312px] text-2xl md:h-[182px] md:w-[384px] lg:h-[236px] lg:w-[640px] lg:text-3xl'
+      />
+      <div className='px-auto flex gap-4 pt-9'>
+        {data?.isLiked ? (
+          <RoundedButton variant='secondary' onClick={handleToggleLike}>
+            <div className='flex items-center justify-center'>
+              <div className='hidden md:block'>
+                <Image src={like} alt='좋아요 따봉 이미지' width={36} height={36} />
+              </div>
+              <div className='block md:hidden'>
+                <Image src={like} alt='좋아요 따봉 이미지' width={20} height={20} />
+              </div>
+              <div className='text-md min-w-[65px] text-center lg:text-xl'>{data?.likeCount}</div>
             </div>
-            <div className='block md:hidden'>
-              <Image src={like} alt='좋아요 따봉 이미지' width={20} height={20} priority />
+          </RoundedButton>
+        ) : (
+          <RoundedButton variant='secondary' onClick={handleToggleLike}>
+            <div className='flex items-center justify-center'>
+              <div className='hidden h-9 w-9 md:block'>
+                <Image src={like_outlined} alt='좋아요 따봉 이미지' className='mt-1.5 ml-1' />
+              </div>
+              <div className='block md:hidden md:h-5 md:w-5'>
+                <Image src={like_outlined} alt='좋아요 따봉 이미지' />
+              </div>
+              <div className='text-md min-w-[65px] text-center lg:text-xl'>{data?.likeCount}</div>
             </div>
-            <div className='text-md min-w-[65px] text-center lg:text-xl'>{likeCount}</div>
-          </div>
-        </RoundedButton>
-      ) : (
-        <RoundedButton variant='secondary' onClick={handleToggleLike}>
-          <div className='flex items-center justify-center'>
-            <div className='hidden h-9 w-9 md:block'>
-              <Image src={like_outlined} alt='좋아요 따봉 이미지' className='mt-1.5 ml-1' priority />
-            </div>
-            <div className='block md:hidden md:h-5 md:w-5'>
-              <Image src={like_outlined} alt='좋아요 따봉 이미지' priority />
-            </div>
-            <div className='text-md min-w-[65px] text-center lg:text-xl'>{likeCount}</div>
-          </div>
-        </RoundedButton>
-      )}
+          </RoundedButton>
+        )}
 
-      <RoundedButton variant='outline' onClick={handleCopyUrl}>
-        <div className='flex items-center justify-center gap-2'>
-          <div className='text-md lg:text-xl'>URL 복사</div>
-          <div className='hidden md:block'>
-            <Image src={share} alt='URL 복사 이미지' width={25} height={25} />
-          </div>
-          <div className='hidden sm:block md:hidden'>
-            <Image src={share} alt='URL 복사 이미지' width={15} height={15} />
-          </div>
-        </div>
-      </RoundedButton>
-      {referenceUrl && (
-        <RoundedButton onClick={() => referenceUrl && window.open(referenceUrl, '_blank')}>
-          <div className='flex items-center justify-center'>
-            <div className='text-md lg:text-xl'>왕도로 가는 길</div>
+        <RoundedButton variant='outline' onClick={handleCopyUrl}>
+          <div className='flex items-center justify-center gap-2'>
+            <div className='text-md lg:text-xl'>URL 복사</div>
             <div className='hidden md:block'>
-              <Image src={external} alt='외부 이동 이미지' width={36} height={36} />
+              <Image src={share} alt='URL 복사 이미지' width={25} height={25} />
             </div>
             <div className='hidden sm:block md:hidden'>
-              <Image src={external} alt='외부 이동 이미지' width={20} height={20} />
+              <Image src={share} alt='URL 복사 이미지' width={15} height={15} />
             </div>
           </div>
         </RoundedButton>
-      )}
+        {hasSourceUrl && (
+          <RoundedButton onClick={() => data.referenceUrl && window.open(data.referenceUrl, '_blank')}>
+            <div className='flex items-center justify-center'>
+              <div className='text-md lg:text-xl'>왕도로 가는 길</div>
+              <div className='hidden md:block'>
+                <Image src={external} alt='외부 이동 이미지' width={36} height={36} />
+              </div>
+              <div className='hidden sm:block md:hidden'>
+                <Image src={external} alt='외부 이동 이미지' width={20} height={20} />
+              </div>
+            </div>
+          </RoundedButton>
+        )}
+      </div>
+      <div className='after:zigzag relative h-4 w-full'></div>
     </div>
   );
 }
