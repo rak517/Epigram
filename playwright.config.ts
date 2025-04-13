9cedc309269d4f77efbd4c4192f0cf7196fa1ed8
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-04-13 12:29:53 +0900

    :bug: fix: 오늘의 감정 E2E 오류 해결 및 CI 단계 설정 수정

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,11 +5,14 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
+    launchOptions: {
+      args: ['--disable-images'],
+    },
   },
   projects: [
     {
